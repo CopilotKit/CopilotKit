74261cd73b61a9a865af83cf6cc573b8e747e2f9
Atai Barkai <atai.barkai@gmail.com>
2023-07-15 19:44:43 -0700

    wip

--- packages/react-core/tsup.config.ts
+++ packages/react-core/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["src/index.tsx"],
+  entry: ["src/**/*.tsx"],
   format: ["esm"],
   dts: true,
   minify: false,
