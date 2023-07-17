e4faea4adfbec9ace11ef6259c3a9dfaba8422af
Atai Barkai <atai.barkai@gmail.com>
2023-07-17 11:47:46 -0700

    breakpoint debugging potentially works better?

--- packages/react-ui/tsup.config.ts
+++ packages/react-ui/tsup.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  treeshake: true,
-  splitting: true,
   entry: ["src/**/*.{ts,tsx}"],
   format: ["esm"],
   dts: true,
