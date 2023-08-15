c5de7de1de41f4451f92210789139d29054a7bba
Atai Barkai <atai.barkai@gmail.com>
2023-08-15 06:30:57 -0700

    add documentation

--- packages/react-core/src/hooks/use-make-copilot-readable.ts
+++ packages/react-core/src/hooks/use-make-copilot-readable.ts
@@ -3,6 +3,13 @@
 import { useRef, useContext, useEffect } from "react";
 import { CopilotContext } from "../context/copilot-context";
 
+/**
+ * Adds the given information to the Copilot context to make it readable by Copilot.
+ * @param information - The information to be added to the Copilot context.
+ * @param parentId - The ID of the parent context, if any.
+ * @param categories - An array of categories to control which context are visible where. Particularly useful with CopilotTextarea (see `useMakeAutosuggestionFunction`)
+ * @returns The ID of the added context.
+ */
 export function useMakeCopilotReadable(
   information: string,
   parentId?: string,
--- packages/react-textarea/src/hooks/use-make-autosuggestions-function.tsx
+++ packages/react-textarea/src/hooks/use-make-autosuggestions-function.tsx
@@ -8,6 +8,19 @@ export interface MinimalChatGPTMessage {
   content: string;
   name?: string;
 }
+
+/**
+ * Returns a memoized function that sends a request to the specified API endpoint to get an autosuggestion for the user's input.
+ * The function takes in the text before and after the cursor, and an abort signal.
+ * It sends a POST request to the API endpoint with the messages array containing the system message, few shot messages, and user messages.
+ * The function returns the suggestion from the API response.
+ *
+ * @param apiEndpoint - The API endpoint to send the autosuggestion request to.
+ * @param makeSystemMessage - A function that takes in a context string and returns a system message to include in the autosuggestion request.
+ * @param fewShotMessages - An array of few shot messages to include in the autosuggestion request.
+ * @param contextCategories - The categories of context strings we want to include. By default, we include the (default) "global" context category.
+ * @returns A memoized function that sends a request to the specified API endpoint to get an autosuggestion for the user's input.
+ */
 export function useMakeAutosuggestionFunction(
   apiEndpoint: string = "/api/autosuggest",
   makeSystemMessage: (message: string) => string = defaultMakeSystemMessage,
