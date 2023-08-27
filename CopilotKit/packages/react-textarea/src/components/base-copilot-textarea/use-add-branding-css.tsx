4de27ae22270bcfd5ae0a30c474ef130c110c6c6
Atai Barkai <atai.barkai@gmail.com>
2023-08-27 09:55:18 -0700

    move all critical css to dynamic part so not overriden. and align symetrically

--- CopilotKit/packages/react-textarea/src/components/base-copilot-textarea/use-add-branding-css.tsx
+++ CopilotKit/packages/react-textarea/src/components/base-copilot-textarea/use-add-branding-css.tsx
@@ -25,6 +25,14 @@ export function useAddBrandingCss(
     // Append overrides for italics and font-size
     dynamicStyles += `font-style: normal; font-size: x-small;`;
     dynamicStyles += `content: "CopilotKit";`;
+    dynamicStyles += `bottom: 6px;`;
+    dynamicStyles += `right: 6px;`;
+    dynamicStyles += `pointer-events: none;`;
+    dynamicStyles += `font-weight: 200;`;
+    dynamicStyles += `padding: 0;`;
+    dynamicStyles += `margin: 0;`;
+    dynamicStyles += `border: 0;`;
+    dynamicStyles += `line-height: 1;`;
 
     // Append it to the ::after class
     styleEl.innerHTML = `
