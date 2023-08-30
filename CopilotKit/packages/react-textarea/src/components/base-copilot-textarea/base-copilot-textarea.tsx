5b677816fad3eed916aa7d2a93af1df27f3a2816
Atai Barkai <atai.barkai@gmail.com>
2023-08-30 10:33:44 -0700

    usePopulateCopilotTextareaRef

--- CopilotKit/packages/react-textarea/src/components/base-copilot-textarea/base-copilot-textarea.tsx
+++ CopilotKit/packages/react-textarea/src/components/base-copilot-textarea/base-copilot-textarea.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { Descendant, Editor } from "slate";
-import { Editable, ReactEditor, Slate } from "slate-react";
+import { Editable, Slate } from "slate-react";
 import { twMerge } from "tailwind-merge";
 import { useAutosuggestions } from "../../hooks/base-copilot-textarea-implementation/use-autosuggestions";
 import { useCopilotTextareaEditor } from "../../hooks/base-copilot-textarea-implementation/use-copilot-textarea-editor";
+import { usePopulateCopilotTextareaRef } from "../../hooks/base-copilot-textarea-implementation/use-populate-copilot-textarea-ref";
 import {
   getFullEditorTextWithNewlines,
   getTextAroundCursor,
@@ -154,96 +155,7 @@ export const BaseCopilotTextarea = React.forwardRef(
       return `${baseClassName} ${brandingClass} ${mergedClassName}`;
     })();
 
-    React.useImperativeHandle(
-      ref,
-      () => {
-        class Combined {
-          constructor(
-            private customMethods: CustomMethods,
-            private editorHtmlElement: HTMLElement
-          ) {}
-
-          [key: string]: any;
-
-          get(target: any, propKey: string): any {
-            if (this.isKeyOfCustomMethods(propKey)) {
-              const value = this.customMethods[propKey];
-              if (typeof value === "function") {
-                return value.bind(this.customMethods);
-              }
-              return value;
-            } else if (this.isKeyOfHTMLElement(propKey)) {
-              const value = this.editorHtmlElement[propKey];
-              if (typeof value === "function") {
-                return value.bind(this.editorHtmlElement);
-              }
-              return value;
-            }
-          }
-
-          set(target: any, propKey: string, value: any): boolean {
-            if (this.isKeyOfCustomMethods(propKey)) {
-              (this.customMethods as any)[propKey] = value;
-            } else if (this.isKeyOfHTMLElement(propKey)) {
-              (this.editorHtmlElement as any)[propKey] = value;
-            } else {
-              // Default behavior (optional)
-              target[propKey] = value;
-            }
-            return true;
-          }
-
-          private isKeyOfCustomMethods(
-            key: string
-          ): key is keyof CustomMethods {
-            return key in this.customMethods;
-          }
-
-          private isKeyOfHTMLElement(key: string): key is keyof HTMLElement {
-            return key in this.editorHtmlElement;
-          }
-        }
-
-        const handler = {
-          get(target: any, propKey: keyof CustomMethods | keyof HTMLElement) {
-            return target.get(target, propKey);
-          },
-          set(
-            target: any,
-            propKey: keyof CustomMethods | keyof HTMLElement,
-            value: any
-          ) {
-            return target.set(target, propKey, value);
-          },
-        };
-
-        class CustomMethods {
-          constructor(private editor: Editor) {}
-
-          focus() {
-            ReactEditor.focus(this.editor);
-          }
-
-          blur() {
-            ReactEditor.blur(this.editor);
-          }
-
-          get value() {
-            return getFullEditorTextWithNewlines(this.editor);
-          }
-          set value(value: string) {
-            replaceEditorText(this.editor, value);
-          }
-        }
-
-        const editorHtmlElement = ReactEditor.toDOMNode(editor, editor);
-        const customMethods = new CustomMethods(editor);
-
-        const combined = new Combined(customMethods, editorHtmlElement);
-        return new Proxy(combined, handler);
-      },
-      [editor]
-    );
+    usePopulateCopilotTextareaRef(editor, ref);
 
     return (
       <Slate
