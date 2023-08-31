d04646969ae23c474abb9c9d9dbfe843fe2ec4e0
Atai Barkai <atai.barkai@gmail.com>
2023-08-31 07:15:35 -0700

    explicitly put apiEndpoint in example page, for clarity

--- CopilotKit/examples/next-openai/src/app/components/vacation-notes.tsx
+++ CopilotKit/examples/next-openai/src/app/components/vacation-notes.tsx
@@ -1,4 +1,7 @@
-import { CopilotTextarea } from "@copilotkit/react-textarea";
+import {
+  ChatlikeApiEndpoint,
+  CopilotTextarea,
+} from "@copilotkit/react-textarea";
 import { useState } from "react";
 
 export function VacationNotes(): JSX.Element {
@@ -12,6 +15,9 @@ export function VacationNotes(): JSX.Element {
         onValueChange={(value: string) => setText(value)}
         placeholder="What are your plans for your vacation?"
         autosuggestionsConfig={{
+          apiEndpoint: ChatlikeApiEndpoint.standardOpenAIEndpoint(
+            "/api/autosuggestions"
+          ),
           purposePrompt:
             "Travel notes from the user's previous vacations. Likely written in a colloquial style, but adjust as needed.",
           debounceTime: 700,
