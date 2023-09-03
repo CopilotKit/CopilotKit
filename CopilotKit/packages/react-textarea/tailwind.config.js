8bc82f78d1248cf7e6caba9a4ffbb9c3e5598c38
Atai Barkai <atai.barkai@gmail.com>
2023-09-03 07:10:27 -0700

    Just in time tailwind for live library updates

--- CopilotKit/packages/react-textarea/tailwind.config.js
+++ CopilotKit/packages/react-textarea/tailwind.config.js
@@ -3,5 +3,7 @@ const sharedConfig = require("tailwind-config/tailwind.config.js");
 module.exports = {
   // prefix ui lib classes to avoid conflicting with the app
   // prefix: "ui-",
+  mode: 'jit',
   presets: [sharedConfig],
+  purge: sharedConfig.content,  // Use the 'content' from the shared config for purging
 };
