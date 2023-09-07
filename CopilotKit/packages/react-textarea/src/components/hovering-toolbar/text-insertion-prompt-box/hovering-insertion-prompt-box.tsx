880ffb1fa0260ed44c32828269c7bd19de93d4a9
Atai Barkai <atai.barkai@gmail.com>
2023-09-07 10:57:17 -0700

    split insertion box into 2 modes: pre suggestion, and suggestion appearing

--- CopilotKit/packages/react-textarea/src/components/hovering-toolbar/text-insertion-prompt-box/hovering-insertion-prompt-box.tsx
+++ CopilotKit/packages/react-textarea/src/components/hovering-toolbar/text-insertion-prompt-box/hovering-insertion-prompt-box.tsx
@@ -12,7 +12,6 @@ import {
 import { Label } from "../../ui/label";
 import { Textarea } from "../../ui/textarea";
 import React, { useEffect, useRef, useState } from "react";
-import { BottomButton } from "./bottom-button";
 
 export type InsertTextFunctionRaw = (
   editorState: InsertionEditorState,
@@ -24,6 +23,21 @@ export interface InsertionEditorState {
   textAfterCursor: string;
 }
 
+type State_PreInsertion = {
+  type: "pre-insertion";
+};
+
+type State_SuggestionAppearing = {
+  type: "suggestion-appearing";
+  suggestion: string;
+};
+
+type InsertionPromptState = State_PreInsertion | State_SuggestionAppearing;
+
+interface PreSuggestionProps {
+  editorState: InsertionEditorState;
+}
+
 export interface Props {
   editorState: InsertionEditorState;
   insertionFunction: InsertTextFunctionRaw;
@@ -31,89 +45,117 @@ export interface Props {
   closeWindow: () => void;
 }
 
-export const HoveringInsertionPromptBox: React.FC<Props> = ({
+interface PreInsertionProps {
+  editorState: InsertionEditorState;
+  insertionFunction: InsertTextFunctionRaw;
+  onGeneratedText: (generatedText: string) => void;
+}
+
+const PreInsertion: React.FC<PreInsertionProps> = ({
   editorState,
   insertionFunction,
-  performInsertion,
-  closeWindow,
+  onGeneratedText,
 }) => {
   const [editPrompt, setEditPrompt] = useState("");
-  const [editSuggestion, setEditSuggestion] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
   const promptTextAreaRef = useRef<HTMLTextAreaElement>(null);
   useAutosizeTextArea(promptTextAreaRef, editPrompt);
 
-  const suggestionTextAreaRef = useRef<HTMLTextAreaElement>(null);
-  useAutosizeTextArea(suggestionTextAreaRef, editSuggestion || "");
-
   const generateText = async () => {
     setLoading(true);
     const editedText = await insertionFunction(editorState, editPrompt);
-    setEditSuggestion(editedText);
     setLoading(false);
+    onGeneratedText(editedText);
   };
 
   return (
-    <div className="flex flex-col justify-center items-center space-y-4 rounded-md border w-96 shadow-lg p-4 border-gray- bg-white">
-      <div className="flex justify-center items-start">
-        <textarea
-          ref={promptTextAreaRef}
-          value={editPrompt}
-          onChange={(e) => setEditPrompt(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && e.shiftKey) {
-              e.preventDefault();
-              setEditPrompt(editPrompt + "\n");
-            } else if (e.key === "Enter") {
-              e.preventDefault();
-              generateText();
-            }
-          }}
-          placeholder="Describe the text you'd like to insert..."
-          className="w-full bg-slate-200 h-auto text-sm p-2 rounded-md resize-none overflow-visible focus:outline-none focus:ring-0 focus:border-none"
-          rows={1}
-        />
-        {editSuggestion !== null && (
-          <button
-            className="h-8 right-0 p-3 flex items-center"
-            onClick={generateText}
-          >
-            <i className="material-icons">refresh</i>
-          </button>
-        )}
-      </div>
-
-      {editSuggestion !== null && (
-        <div className="w-full flex flex-col items-start relative gap-2">
-          <Label className="">Suggested:</Label>
-
-          <textarea
-            ref={suggestionTextAreaRef}
-            value={editSuggestion}
-            onChange={(e) => setEditSuggestion(e.target.value)}
-            className="w-full text-base p-2 border border-gray-300 rounded-md resize-none bg-green-200"
-            style={{ overflow: "auto", maxHeight: "8em" }}
-          />
-
-          <div className="text-left w-full text-white">
-            <Button
-              className=" bg-green-700"
-              onClick={() => {
-                performInsertion(editSuggestion);
-              }}
-            >
-              Insert <i className="material-icons">check</i>
-            </Button>
-          </div>
-        </div>
-      )}
-      <BottomButton
-        loading={loading}
-        editSuggestion={editSuggestion}
-        generateText={generateText}
-        performInsertion={performInsertion}
+    <div className="flex flex-col justify-center items-start">
+      <textarea
+        ref={promptTextAreaRef}
+        value={editPrompt}
+        onChange={(e) => setEditPrompt(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && e.shiftKey) {
+            e.preventDefault();
+            setEditPrompt(editPrompt + "\n");
+          } else if (e.key === "Enter") {
+            e.preventDefault();
+            generateText();
+          }
+        }}
+        placeholder="Describe the text you'd like to insert..."
+        className="w-full bg-slate-200 h-auto text-sm p-2 rounded-md resize-none overflow-visible focus:outline-none focus:ring-0 focus:border-none"
+        rows={1}
       />
+      <button
+        disabled={loading}
+        onClick={generateText}
+        className="w-full py-2 px-4 rounded-md text-white bg-blue-500 hover:bg-blue-700"
+      >
+        {loading ? "Loading..." : "Generate Text"}
+      </button>
+    </div>
+  );
+};
+
+interface SuggestionAppearingProps {
+  state: State_SuggestionAppearing;
+  performInsertion: (insertedText: string) => void;
+}
+
+const SuggestionAppearing: React.FC<SuggestionAppearingProps> = ({
+  performInsertion,
+  state,
+}) => {
+  const [editSuggestion, setEditSuggestion] = useState<string>(
+    state.suggestion
+  );
+
+  const suggestionTextAreaRef = useRef<HTMLTextAreaElement>(null);
+  useAutosizeTextArea(suggestionTextAreaRef, editSuggestion || "");
+
+  return (
+    <div className="w-full flex flex-col items-start relative gap-2">
+      <Label className="">Suggested:</Label>
+
+      <textarea
+        ref={suggestionTextAreaRef}
+        value={editSuggestion}
+        onChange={(e) => setEditSuggestion(e.target.value)}
+        className="w-full text-base p-2 border border-gray-300 rounded-md resize-none bg-green-200"
+        style={{ overflow: "auto", maxHeight: "8em" }}
+      />
+
+      <div className="text-left w-full text-white">
+        <Button
+          className=" bg-green-700"
+          onClick={() => {
+            performInsertion(editSuggestion);
+          }}
+        >
+          Insert <i className="material-icons">check</i>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const HoveringInsertionPromptBox: React.FC<Props> = (props) => {
+  const [mode, setMode] = useState<InsertionPromptState>({
+    type: "pre-insertion",
+  });
+
+  const handleGeneratedText = (newGeneratedText: string) => {
+    setMode({ type: "suggestion-appearing", suggestion: newGeneratedText });
+  };
+  return (
+    <div className="flex flex-col justify-center items-center space-y-4 rounded-md border w-96 shadow-lg p-4 border-gray- bg-white">
+      {mode.type === "pre-insertion" ? (
+        <PreInsertion {...props} onGeneratedText={handleGeneratedText} />
+      ) : (
+        <SuggestionAppearing {...props} state={mode} />
+      )}
     </div>
   );
 };
