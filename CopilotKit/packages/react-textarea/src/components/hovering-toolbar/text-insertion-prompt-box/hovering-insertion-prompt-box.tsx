e7beea659e269bcfab7be6389ab4ca49710a46e9
Atai Barkai <atai.barkai@gmail.com>
2023-10-03 15:41:35 -0700

    wip material

--- CopilotKit/packages/react-textarea/src/components/hovering-toolbar/text-insertion-prompt-box/hovering-insertion-prompt-box.tsx
+++ CopilotKit/packages/react-textarea/src/components/hovering-toolbar/text-insertion-prompt-box/hovering-insertion-prompt-box.tsx
@@ -43,7 +43,7 @@ export const HoveringInsertionPromptBox: React.FC<Props> = (props) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center space-y-4 rounded-md border w-96 shadow-lg p-4 border-gray- bg-white">
+    <div className="flex flex-col justify-center items-center space-y-4 rounded-md border shadow-lg p-4 border-gray- bg-white" style={{width: "30rem"}}>
       {mode.type === "pre-suggestion" ? (
         <PreSuggestion
           {...props}
