2f0c155855cbb74c32cabfd0a7ccb775c6554106
Atai Barkai <atai.barkai@gmail.com>
2023-11-27 17:47:59 -0800

    use-make-copilot-document-readable: docs

--- CopilotKit/packages/react-core/src/hooks/use-make-copilot-document-readable.ts
+++ CopilotKit/packages/react-core/src/hooks/use-make-copilot-document-readable.ts
@@ -5,11 +5,11 @@ import { CopilotContext } from "../context/copilot-context";
 import { DocumentPointer } from "../types";
 
 /**
- * Adds the given information to the Copilot context to make it readable by Copilot.
- * @param information - The information to be added to the Copilot context.
- * @param parentId - The ID of the parent context, if any.
- * @param categories - An array of categories to control which context are visible where. Particularly useful with CopilotTextarea (see `useMakeAutosuggestionFunction`)
- * @returns The ID of the added context.
+ * Makes a document readable by Copilot.
+ * @param document The document to make readable.
+ * @param categories The categories to associate with the document.
+ * @param dependencies The dependencies to use for the effect.
+ * @returns The id of the document.
  */
 export function useMakeCopilotDocumentReadable(
   document: DocumentPointer,
