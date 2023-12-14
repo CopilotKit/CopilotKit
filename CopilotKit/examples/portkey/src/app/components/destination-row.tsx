7c0c921bfe536132a80d03a02ff1f1b3b031b5e9
vrushankportkey <vrushank.v@portkey.ai>
2023-12-14 16:42:17 +0530

    Add Portkey to docs & examples

---
+++ CopilotKit/examples/portkey/src/app/components/destination-row.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Destination } from "./vacation-list";
+import { useMakeCopilotReadable } from "@copilotkit/react-core";
+
+export type DestinationRowProps = {
+  destination: Destination;
+  isChecked: boolean;
+  onCheckChange: (isChecked: boolean) => void;
+  parentCopilotPointer?: string;
+};
+
+export function DestinationRow({
+  destination,
+  isChecked,
+  onCheckChange,
+  parentCopilotPointer,
+}: DestinationRowProps) {
+  useMakeCopilotReadable(
+    JSON.stringify({
+      name: destination.name,
+      country: destination.country,
+      description: destination.description,
+      activities: destination.activities,
+      isSelected: isChecked,
+    }),
+    parentCopilotPointer,
+  );
+
+  return (
+    <tr key={destination.name}>
+      <td className="whitespace-nowrap py-5 pl-4 px-3 text-sm">
+        <div className="flex items-center">
+          <div className="h-20 w-20 flex-shrink-0">
+            <img className="h-full w-full rounded-full" src={destination.image} alt="" />
+          </div>
+          <div className="ml-4">
+            <div className="font-medium text-gray-900">{destination.name}</div>
+            <div className="mt-1 text-gray-500">{destination.country}</div>
+          </div>
+        </div>
+      </td>
+      <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm">
+        <div className="w-full flex items-stretch">
+          <div className="flex items-center justify-center w-full">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={(event) => onCheckChange(event.target.checked)}
+            />
+          </div>
+        </div>
+      </td>
+      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+        <div className="text-gray-900">{destination.description}</div>
+      </td>
+      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+        {destination.activities}
+      </td>
+    </tr>
+  );
+}
