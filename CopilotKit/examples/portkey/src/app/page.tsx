2af53178c062fc0997e4b18e6e3cf446b65f1417
vrushankportkey <vrushank.v@portkey.ai>
2023-12-14 16:29:55 +0530

    Add Portkey to examples

---
+++ CopilotKit/examples/portkey/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { CopilotProvider } from "@copilotkit/react-core";
+import { CopilotSidebarUIProvider } from "@copilotkit/react-ui";
+import { VacationList } from "./components/vacation-list";
+
+export default function WaterBnb(): JSX.Element {
+  return (
+    <CopilotProvider chatApiEndpoint="/api/copilotkit/chat">
+      <CopilotSidebarUIProvider>
+        <VacationList />
+      </CopilotSidebarUIProvider>
+    </CopilotProvider>
+  );
+}
