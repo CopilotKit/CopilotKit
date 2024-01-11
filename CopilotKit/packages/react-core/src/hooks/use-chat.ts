e8f048e8a4759eccb2b5bb58df1c46c70c21e9ca
Markus Ecker <markus.ecker@gmail.com>
2024-01-11 13:17:15 +0100

    Add useChat skeleton

---
+++ CopilotKit/packages/react-core/src/hooks/use-chat.ts
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Message, FunctionCallHandler } from "../types/message";
+
+export type UseChatOptions = {
+  /**
+   * The API endpoint that accepts a `{ messages: Message[] }` object and returns
+   * a stream of tokens of the AI chat response. Defaults to `/api/chat`.
+   */
+  api?: string;
+  /**
+   * A unique identifier for the chat. If not provided, a random one will be
+   * generated. When provided, the `useChat` hook with the same `id` will
+   * have shared states across components.
+   */
+  id?: string;
+  /**
+   * System messages of the chat. Defaults to an empty array.
+   */
+  systemMessages?: Message[];
+  /**
+   * Callback function to be called when a function call is received.
+   * If the function returns a `ChatRequest` object, the request will be sent
+   * automatically to the API and will be used to update the chat.
+   */
+  onFunctionCall?: FunctionCallHandler;
+  /**
+   * HTTP headers to be sent with the API request.
+   */
+  headers?: Record<string, string> | Headers;
+  /**
+   * Extra body object to be sent with the API request.
+   * @example
+   * Send a `sessionId` to the API along with the messages.
+   * ```js
+   * useChat({
+   *   body: {
+   *     sessionId: '123',
+   *   }
+   * })
+   * ```
+   */
+  body?: object;
+};
+
+type UseChatHelpers = {
+  /** Current messages in the chat */
+  messages: Message[];
+  /**
+   * Append a user message to the chat list. This triggers the API call to fetch
+   * the assistant's response.
+   * @param message The message to append
+   */
+  append: (message: Message) => Promise<void>;
+  /**
+   * Reload the last AI chat response for the given chat history. If the last
+   * message isn't from the assistant, it will request the API to generate a
+   * new response.
+   */
+  reload: () => Promise<void>;
+  /**
+   * Abort the current request immediately, keep the generated tokens if any.
+   */
+  stop: () => void;
+  /** The current value of the input */
+  input: string;
+  /** setState-powered method to update the input value */
+  setInput: React.Dispatch<React.SetStateAction<string>>;
+  /** Whether the API request is in progress */
+  isLoading: boolean;
+};
+
+export function useChat(options: UseChatOptions): UseChatHelpers {
+  const [input, setInput] = useState("");
+
+  throw new Error("Not implemented");
+}
