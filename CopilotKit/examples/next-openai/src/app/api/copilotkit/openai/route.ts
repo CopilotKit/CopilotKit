febca8fa3f37a549d0470041e80e4d2ee4520b52
Markus Ecker <markus.ecker@gmail.com>
2024-01-11 18:15:49 +0100

    remove vercel from the server

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
@@ -1,6 +1,4 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
 import OpenAI from "openai";
-import { ChatCompletionCreateParamsStreaming } from "openai/resources/chat/completions";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -12,22 +10,15 @@ export async function POST(req: Request): Promise<Response> {
   try {
     const forwardedProps = await req.json();
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-4-1106-preview",
-      ...forwardedProps,
-      stream: true,
-    } as ChatCompletionCreateParamsStreaming);
+    const stream = openai.beta.chat.completions
+      .stream({
+        model: "gpt-4-1106-preview",
+        ...forwardedProps,
+        stream: true,
+      })
+      .toReadableStream();
 
-    const stream = OpenAIStream(response, {
-      experimental_onFunctionCall: async (
-        { name, arguments: args },
-        createFunctionCallMessages,
-      ) => {
-        return undefined; // returning undefined to avoid sending any messages to the client when a function is called. Temporary, bc currently vercel ai sdk does not support returning both text and function calls -- although the API does support it.
-      },
-    });
-
-    return new StreamingTextResponse(stream);
+    return new Response(stream);
   } catch (error) {
     return new Response("", { status: 500, statusText: error.error.message });
   }
