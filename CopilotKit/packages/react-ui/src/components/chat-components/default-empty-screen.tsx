3016a823139b7c063d27c4ed9f8854389e3a6d27
Atai Barkai <atai.barkai@gmail.com>
2024-01-13 00:02:07 -0800

    only export UseChatHelpers out of react-core

--- CopilotKit/packages/react-ui/src/components/chat-components/default-empty-screen.tsx
+++ CopilotKit/packages/react-ui/src/components/chat-components/default-empty-screen.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { UseChatHelpers } from "@copilotkit/react-core";
 
 import { Button } from "./ui/button";
 import { ExternalLink } from "./external-link";
