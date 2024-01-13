403da7a50ff173a417449fd8dfb3ede2171c2d83
Atai Barkai <atai.barkai@gmail.com>
2024-01-13 08:46:41 -0800

    textarea editing prompt default prompt + few shot update

--- CopilotKit/packages/react-textarea/src/types/autosuggestions-config/editing-api-config.tsx
+++ CopilotKit/packages/react-textarea/src/types/autosuggestions-config/editing-api-config.tsx
@@ -11,22 +11,23 @@ export const defaultEditingMakeSystemPrompt: MakeSystemPrompt = (
   textareaPurpose,
   contextString,
 ) => {
-  return `You are a versatile writing assistant.
+  return `You are a versatile writing assistant helping the user edit a portion of their text.
   
 The user is writing some text.
 The purpose is: \"${textareaPurpose}\"
 
-The following external context is also provided. Use it to help you make better suggestions!!!
+The following external context is also provided. Use it when relevant.
 \`\`\`
 ${contextString}
 \`\`\`
 
-The user also provides you with a prompt for EDITING some text they are writing. 
-Your job is to come up with an EDIT of the text that the user would like to use - AS BEST YOU CAN.
+The user has provided you with a PROMPT for EDITING a PORTION of the text. 
+Your job is to come up with a new EDITED version OF THE SEGMENT IN QUESTION - AS BEST YOU CAN.
+Only rewrite the portion of the text that the user has marked as "TextToEdit"!!!
 
 Adjust yourself to the user's style and implied intent.
 
-The user will provide the following information; use this to infer the best relevant EDIT:
+The conversation will be structured as follows:
 <TextBeforeCursor>
 <TextToEdit>
 <TextAfterCursor>
@@ -45,7 +46,7 @@ export const defaultEditingFewShotMessages: MinimalChatGPTMessage[] = [
   {
     role: "user",
     name: "TextToEdit",
-    content: "While I was there I also picked up some apples, oranges, and bananas. ",
+    content: "While I was there I picked up some apples, oranges, and bananas. ",
   },
   {
     role: "user",
@@ -55,34 +56,38 @@ export const defaultEditingFewShotMessages: MinimalChatGPTMessage[] = [
   {
     role: "user",
     name: "EditingPrompt",
-    content: "I bought a big watermelon",
+    content: "I also bought a big watermelon",
   },
   {
     role: "assistant",
     content:
-      " When I arrived I went straight to the produce section and picked out a big watermelon. ",
+      "While I was there I picked up some apples, oranges, and bananas, and a big watermelon.",
+  },
+
+  {
+    role: "user",
+    name: "TextBeforeCursor",
+    content: "Yesterday, I spent the afternoon working on my new project. ",
+  },
+  {
+    role: "user",
+    name: "TextToEdit",
+    content: "It's quite challenging and requires a lot of focus. ",
   },
   {
     role: "user",
     name: "TextAfterCursor",
-    content:
-      "and (iii) to the appointment of the Equityholders' Representative pursuant to Section 10.7 of the Merger Agreement and to the provisions thereof.",
+    content: "I'm really excited about the potential outcomes of this project.",
   },
   {
     role: "user",
-    name: "TextBeforeCursor",
-    content:
-      'The Optionholder, in the Optionholder\'s capacity as a holder of vested Options, hereby irrevocably and unconditionally agrees: (i) that the Optionholder shall be deemed an "Equityholder" under the Merger Agreement and shall be entitled to the rights and benefits, and subject to the obligations, of an "Equityholder" thereunder;',
-  },
-  {
-    role: "user",
-    name: "InsertionPrompt",
-    content: "add section about the optionholder's pro rata share",
+    name: "EditingPrompt",
+    content: "emphasize the complexity and my enthusiasm for the project",
   },
   {
     role: "assistant",
     content:
-      ' (ii) that, for purposes of this Agreement and the Merger Agreement, the applicable percentage set forth opposite the name of the Optionholder in the Distribution Waterfall shall be such the Optionholder\'s "Pro Rata Share"; ',
+      "It's a highly complex task that demands intense concentration, but I'm incredibly enthusiastic about the promising prospects of this project.",
   },
 ];
 
