16555c50e30a71c7bc36718b6ef8586641b849fd
Markus Ecker <markus.ecker@gmail.com>
2024-01-17 12:13:51 +0100

    Show at least the done label when a function call was made

--- CopilotKit/packages/react-ui/src/components/chat/Messages.tsx
+++ CopilotKit/packages/react-ui/src/components/chat/Messages.tsx
@@ -44,6 +44,12 @@ export const Messages: React.FC<MessagesProps> = ({ messages, inProgress }) => {
                 {context.icons.spinnerIcon}
               </div>
             );
+          } else if (!inProgress && !message.content && message.function_call) {
+            return (
+              <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
+                {context.labels.done}
+              </div>
+            );
           }
           // TODO: Add back partial message
           // This shows up when the assistant is executing a function
