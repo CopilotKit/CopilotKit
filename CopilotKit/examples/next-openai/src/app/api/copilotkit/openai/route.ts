32aea82b44dc0397c7ef9fa24cc097d90f23a7a4
Markus Ecker <markus.ecker@gmail.com>
2024-01-22 15:52:59 +0100

    add convenience functions

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
@@ -3,29 +3,25 @@ import { CopilotKitBackend } from "@copilotkit/backend";
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
-  try {
-    const copilotKit = new CopilotKitBackend({
-      functions: [
-        {
-          name: "sayHello",
-          description: "Says hello to someone.",
-          argumentAnnotations: [
-            {
-              name: "arg",
-              type: "string",
-              description: "The name of the person to say hello to.",
-              required: true,
-            },
-          ],
-          implementation: async (arg) => {
-            console.log("Hello from the server", arg, "!");
+  const copilotKit = new CopilotKitBackend({
+    functions: [
+      {
+        name: "sayHello",
+        description: "Says hello to someone.",
+        argumentAnnotations: [
+          {
+            name: "arg",
+            type: "string",
+            description: "The name of the person to say hello to.",
+            required: true,
           },
+        ],
+        implementation: async (arg) => {
+          console.log("Hello from the server", arg, "!");
         },
-      ],
-    });
+      },
+    ],
+  });
 
-    return new Response(copilotKit.stream(await req.json()));
-  } catch (error) {
-    return new Response("", { status: 500, statusText: error.error.message });
-  }
+  return copilotKit.response(req);
 }
--- CopilotKit/packages/backend/src/lib/copilotkit-backend.ts
+++ CopilotKit/packages/backend/src/lib/copilotkit-backend.ts
@@ -13,10 +13,30 @@ export class CopilotKitBackend {
     this.functions = params?.functions || [];
   }
 
+  addFunction(func: AnnotatedFunction<any[]>): void {
+    this.removeFunction(func.name);
+    this.functions.push(func);
+  }
+
+  removeFunction(funcName: string): void {
+    this.functions = this.functions.filter((f) => f.name !== funcName);
+  }
+
   stream(
     forwardedProps: any,
     serviceAdapter: CopilotKitServiceAdapter = new OpenAIAdapter(),
   ): ReadableStream {
     return serviceAdapter.stream(this.functions, forwardedProps);
+  }
+
+  async response(
+    req: Request,
+    serviceAdapter: CopilotKitServiceAdapter = new OpenAIAdapter(),
+  ): Promise<Response> {
+    try {
+      return new Response(this.stream(await req.json(), serviceAdapter));
+    } catch (error: any) {
+      return new Response("", { status: 500, statusText: error.error.message });
+    }
   }
 }
