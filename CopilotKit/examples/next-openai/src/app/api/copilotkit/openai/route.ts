39bd5b2280b3f84f0e477202e2703a60733da773
Markus Ecker <markus.ecker@gmail.com>
2024-02-07 19:04:57 +0100

    roast the user

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
@@ -1,25 +1,42 @@
 import { CopilotBackend, OpenAIAdapter } from "@copilotkit/backend";
+import { inferLangServeParameters } from "@copilotkit/backend";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
 
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
   const copilotKit = new CopilotBackend({
     actions: [
-      { chainUrl: "http://localhost:8000", name: "xyz", description: "xyz" },
+      await inferLangServeParameters({
+        chainUrl: "http://localhost:8000/agent",
+        name: "askTheAgent",
+        description: "Ask the agent about something",
+      }),
       {
         name: "sayHello",
         description: "Says hello to someone.",
         argumentAnnotations: [
           {
-            name: "arg",
+            name: "name",
             type: "string",
             description: "The name of the person to say hello to.",
             required: true,
           },
         ],
-        implementation: async (arg) => {
-          console.log("Hello from the server", arg, "!");
-          return `I said hello to ${arg}! Give the user feedback on this in a funny way!`;
+        implementation: async (name) => {
+          const prompt = ChatPromptTemplate.fromMessages([
+            [
+              "system",
+              "The user tells you their name. Say hello to the person in the most " +
+                " ridiculous way, roasting their name.",
+            ],
+            ["user", "My name is {name}"],
+          ]);
+          const chain = prompt.pipe(new ChatOpenAI());
+          return chain.invoke({
+            name: name,
+          });
         },
       },
     ],
