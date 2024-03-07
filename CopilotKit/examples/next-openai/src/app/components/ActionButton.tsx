850cfc1ce7927246e55296b325604de5d0da432c
Markus Ecker <markus.ecker@gmail.com>
2024-03-07 12:50:56 +0100

    add presentation UI

---
+++ CopilotKit/examples/next-openai/src/app/components/ActionButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export const ActionButton = ({
+  disabled,
+  onClick,
+  className,
+  children,
+}: {
+  disabled: boolean;
+  onClick: () => void;
+  className?: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <button
+      disabled={disabled}
+      className={`bg-blue-500 text-white font-bold py-2 px-4 rounded
+      ${disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-700"}
+      ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
