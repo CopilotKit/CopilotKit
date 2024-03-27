5596e0bd384355624246287a7491f0b3dc5ade11
Markus Ecker <markus.ecker@gmail.com>
2024-03-27 11:45:03 +0100

    clean up docs and code

--- CopilotKit/packages/react-ui/src/components/chat/Messages.tsx
+++ CopilotKit/packages/react-ui/src/components/chat/Messages.tsx
@@ -7,7 +7,7 @@ import { Markdown } from "./Markdown";
 import { useCopilotContext } from "@copilotkit/react-core";
 
 export const Messages = ({ messages, inProgress }: MessagesProps) => {
-  const { entryPoints, chatComponentsCache } = useCopilotContext();
+  const { chatComponentsCache } = useCopilotContext();
   const context = useChatContext();
   const initialMessages = useMemo(
     () => makeInitialMessages(context.labels.initial),
@@ -148,82 +148,7 @@ export const Messages = ({ messages, inProgress }: MessagesProps) => {
               <Markdown content={message.content} />
             </div>
           );
-
-          // if (isCurrentMessage && inProgress && !message.content) {
-          //   // let inProgressLabel = "";
-
-          //   if (message.partialFunctionCall) {
-          //     for (const action of Object.values(entryPoints)) {
-          //       if (
-          //         (action as any).name === message.partialFunctionCall.name &&
-          //         (action as any).render
-          //       ) {
-          //         //       // the label is a function, call it with the arguments
-          //         //       if (typeof action.inProgressLabel === "function") {
-          //         //         inProgressLabel = action.inProgressLabel(
-          //         //           message.partialFunctionCall.arguments as any,
-          //         //           // if function_call is undefined, the arguments are incomplete
-          //         //           message.function_call !== undefined,
-          //         //         );
-          //         //       }
-          //         //       // the label is a string
-          //         //       else {
-          //         //         // (don't do an additional type check so we get a compile error if we add a new type)
-          //         //         inProgressLabel = action.inProgressLabel;
-          //         //       }
-          //       }
-          //     }
-          //   }
-          //   return (
-          //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-          //       {context.icons.spinnerIcon}
-          //       {/* {inProgressLabel && <span className="inProgressLabel">{inProgressLabel}</span>} */}
-          //     </div>
-          //   );
-          // } else if (
-          //   (!inProgress || index != messages.length - 1) &&
-          //   !message.content &&
-          //   message.function_call
-          // ) {
-          //   return (
-          //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-          //       {context.labels.done}
-          //     </div>
-          //   );
-          // }
-          // TODO: Add back partial message
-          // This shows up when the assistant is executing a function
-          //
-          // else if (message.status === "partial") {
-          //   return (
-          //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-          //       {context.labels.thinking} {context.icons.spinnerIcon}
-          //     </div>
-          //   );
-          // }
-          // else {
-          //   return (
-          //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-          //       <Markdown content={message.content} />
-          //     </div>
-          //   );
-          // }
         }
-        // TODO: Add back function and error messages
-        //
-        // else if (message.role === "function" && message.status === "success") {
-        //   return (
-        //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-        //       {context.labels.done}
-        //     </div>
-        //   );
-        // } else if (message.status === "error") {
-        //   return (
-        //     <div key={index} className={`copilotKitMessage copilotKitAssistantMessage`}>
-        //       {context.labels.error}
-        //     </div>
-        //   );
-        // }
       })}
       <div ref={messagesEndRef} />
     </div>
