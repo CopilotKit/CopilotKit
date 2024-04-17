c7ebaa99e21af1b5c245da22632f0bb8d59a7c37
Markus Ecker <markus.ecker@gmail.com>
2024-04-17 20:25:58 +0200

    update domain

--- CopilotKit/packages/backend/src/lib/cloud/constants.ts
+++ CopilotKit/packages/backend/src/lib/cloud/constants.ts
@@ -1,2 +1,1 @@
-export const COPILOT_CLOUD_API_URL =
-  "http://a652952b511104231a6381e0e7bda717-1619073240.us-east-1.elb.amazonaws.com";
+export const COPILOT_CLOUD_API_URL = "https://cloud.copilotkit.ai";
