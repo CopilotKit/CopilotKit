e15c6251e25832c0635aeeb82b44aba4e7eeb3af
Markus Ecker <markus.ecker@gmail.com>
2024-04-24 16:15:00 +0200

    rename cloud chat endpoint

--- CopilotKit/examples/next-openai/src/app/presentation/page.tsx
+++ CopilotKit/examples/next-openai/src/app/presentation/page.tsx
@@ -11,7 +11,7 @@ export default function AIPresentation() {
   return (
     <CopilotKit
       publicApiKey="co-public-7f1efedaa3c2d0da35a7d14bd8595373"
-      url="http://localhost:4000/copilotkit/engine/openai"
+      url="http://localhost:4000/copilotkit/chat/openai"
       cloudRestrictToTopic={{
         validTopics: ["presentation", "music"],
         invalidTopics: ["math", "science", "history"],
