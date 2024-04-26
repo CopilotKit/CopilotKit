8e6f6d8101f660c85bd7bf0d4c85069e00e53125
Markus Ecker <markus.ecker@gmail.com>
2024-04-26 10:06:04 +0200

    update endpoint url

--- CopilotKit/examples/next-openai/src/app/presentation/page.tsx
+++ CopilotKit/examples/next-openai/src/app/presentation/page.tsx
@@ -10,7 +10,8 @@ export default function AIPresentation() {
 
   return (
     <CopilotKit
-      publicApiKey="co-public-033cc359ffc5e0991c3e5a0ac434ccca"
+      publicApiKey="co-public-7f1efedaa3c2d0da35a7d14bd8595373"
+      url="http://localhost:4000/v1/copilotkit/runtime/chat"
       cloudRestrictToTopic={{
         validTopics: ["presentation", "music"],
         invalidTopics: ["math", "science", "history"],
