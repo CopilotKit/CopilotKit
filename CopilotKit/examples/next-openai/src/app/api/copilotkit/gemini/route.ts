a2eba1b315268f18cc3a234b201d8502a652185b
Markus Ecker <markus.ecker@gmail.com>
2024-05-17 19:30:22 +0200

    update docs

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/gemini/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/gemini/route.ts
@@ -1,12 +1,9 @@
 import { CopilotRuntime, GoogleGenerativeAIAdapter } from "@copilotkit/backend";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export const runtime = "edge";
-
 export async function POST(req: Request): Promise<Response> {
   const copilotKit = new CopilotRuntime();
-  const apiKey = process.env["GOOGLE_API_KEY"]!;
-  const genAI = new GoogleGenerativeAI(apiKey);
+  const genAI = new GoogleGenerativeAI(process.env["GOOGLE_API_KEY" + ""]!);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
   return copilotKit.response(req, new GoogleGenerativeAIAdapter({ model }));
