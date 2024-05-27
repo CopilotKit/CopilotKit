05397bb3a8d849d6010f3e467c4327fcd86886f8
Markus Ecker <markus.ecker@gmail.com>
2024-05-27 09:53:14 +0200

    make tavily optional for running the travel demo

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/travel/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/travel/route.ts
@@ -4,25 +4,28 @@ import { research } from "./tavily";
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
-  const copilotKit = new CopilotRuntime({
-    actions: [
-      {
-        name: "research",
-        description:
-          "Call this function to conduct research on a certain topic. Respect other notes about when to call this function",
-        parameters: [
-          {
-            name: "topic",
-            type: "string",
-            description: "The topic to research. 5 characters or longer.",
-          },
-        ],
-        handler: async ({ topic }) => {
-          console.log("Researching topic: ", topic);
-          return await research(topic);
+  const actions: any[] = [];
+
+  if ("TAVILY_API_KEY" in process.env) {
+    actions.push({
+      name: "research",
+      description:
+        "Call this function to conduct research on a certain topic. Respect other notes about when to call this function",
+      parameters: [
+        {
+          name: "topic",
+          type: "string",
+          description: "The topic to research. 5 characters or longer.",
         },
+      ],
+      handler: async ({ topic }) => {
+        console.log("Researching topic: ", topic);
+        return await research(topic);
       },
-    ],
+    });
+  }
+  const copilotKit = new CopilotRuntime({
+    actions: actions,
   });
 
   return copilotKit.response(req, new OpenAIAdapter());
