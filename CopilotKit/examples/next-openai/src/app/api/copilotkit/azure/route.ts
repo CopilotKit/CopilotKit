5891870a862eb1b9d03297646a3bb3dbfea5744f
Markus Ecker <markus.ecker@gmail.com>
2024-05-28 15:13:44 +0200

    Add azure example

---
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/azure/route.ts
@@ -0,0 +1,30 @@
+import { CopilotRuntime, OpenAIAdapter } from "@copilotkit/backend";
+import OpenAI from "openai";
+
+export const runtime = "edge";
+
+export async function POST(req: Request): Promise<Response> {
+  const copilotKit = new CopilotRuntime();
+
+  // The name of your Azure OpenAI Resource.
+  // https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#create-a-resource
+  const resource = "<your resource name>";
+
+  // Corresponds to your Model deployment within your OpenAI resource, e.g. my-gpt35-16k-deployment
+  // Navigate to the Azure OpenAI Studio to deploy a model.
+  const model = "<your model>";
+
+  const apiKey = process.env["AZURE_OPENAI_API_KEY"];
+  if (!apiKey) {
+    throw new Error("The AZURE_OPENAI_API_KEY environment variable is missing or empty.");
+  }
+
+  const openai = new OpenAI({
+    apiKey,
+    baseURL: `https://${resource}.openai.azure.com/openai/deployments/${model}`,
+    defaultQuery: { "api-version": "2023-06-01-preview" },
+    defaultHeaders: { "api-key": apiKey },
+  });
+
+  return copilotKit.response(req, new OpenAIAdapter({ openai }));
+}
