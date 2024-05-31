b37b33223181f5fa57c8038646da7f590fd759ce
Markus Ecker <markus.ecker@gmail.com>
2024-05-31 15:41:08 +0200

    regenerate css variables

--- CopilotKit/packages/react-ui/src/types/css.ts
+++ CopilotKit/packages/react-ui/src/types/css.ts
@@ -9,24 +9,6 @@ export interface CopilotKitCSSProperties extends CSSProperties {
   "--copilot-kit-muted-color"?: string;
   "--copilot-kit-separator-color"?: string;
   "--copilot-kit-scrollbar-color"?: string;
-  "--copilot-kit-button-background-color"?: string;
-  "--copilot-kit-button-icon-color"?: string;
-  "--copilot-kit-header-background-color"?: string;
-  "--copilot-kit-header-title-color"?: string;
-  "--copilot-kit-header-close-button-color"?: string;
-  "--copilot-kit-header-separator-color"?: string;
-  "--copilot-kit-input-background-color"?: string;
-  "--copilot-kit-input-send-button-color"?: string;
-  "--copilot-kit-input-send-button-disabled-color"?: string;
-  "--copilot-kit-input-placeholder-color"?: string;
-  "--copilot-kit-input-color"?: string;
-  "--copilot-kit-input-separator-color"?: string;
-  "--copilot-kit-messages-background-color"?: string;
-  "--copilot-kit-message-user-background-color"?: string;
-  "--copilot-kit-message-user-color"?: string;
-  "--copilot-kit-message-assistant-background-color"?: string;
-  "--copilot-kit-message-assistant-color"?: string;
-  "--copilot-kit-response-button-border-color"?: string;
   "--copilot-kit-response-button-color"?: string;
   "--copilot-kit-response-button-background-color"?: string;
 }
