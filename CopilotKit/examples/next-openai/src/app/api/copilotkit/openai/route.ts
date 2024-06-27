fa942c5ba4b6c3902d273ae13f9d0492e32d11a0
Markus Ecker <markus.ecker@gmail.com>
2024-06-27 16:21:27 +0200

    clean up

--- CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
+++ CopilotKit/examples/next-openai/src/app/api/copilotkit/openai/route.ts
@@ -6,50 +6,7 @@ import {
 import { NextRequest } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  const runtime = new CopilotRuntime({
-    // debug: true,
-    // actions: [
-    //   {
-    //     name: "sayHello",
-    //     description: "Says hello to someone.",
-    //     parameters: [
-    //       {
-    //         name: "name",
-    //         type: "string",
-    //         description: "The name of the person to say hello to.",
-    //         required: true,
-    //       },
-    //     ],
-    //     handler: async ({ name }) => {
-    //       const prompt = ChatPromptTemplate.fromMessages([
-    //         [
-    //           "system",
-    //           "The user tells you their name. Say hello to the person in the most " +
-    //             " ridiculous way, roasting their name.",
-    //         ],
-    //         ["user", "My name is {name}"],
-    //       ]);
-    //       const chain = prompt.pipe(new ChatOpenAI());
-    //       return chain.invoke({
-    //         name: name,
-    //       });
-    //     },
-    //   },
-    // ],
-    // langserve: [
-    //   {
-    //     chainUrl: "http://localhost:8000/retriever",
-    //     name: "askAboutAnimals",
-    //     description: "Always call this function if the users asks about a certain animal.",
-    //   },
-    //   {
-    //     chainUrl: "http://localhost:8000/agent",
-    //     name: "askAboutEugeneThoughts",
-    //     description:
-    //       "Always call this function if the users asks about Eugene's thoughts on a certain topic.",
-    //   },
-    // ],
-  });
+  const runtime = new CopilotRuntime({});
 
   const serviceAdapter = new OpenAIAdapter();
 
