fa107627f634b7d0d0ab78fe5adcf26452f741aa
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-04 16:50:01 +0200

    feat(runtime): add metadata property to copilot request input

--- CopilotKit/packages/runtime-client-gql/src/client/CopilotRuntimeClient.ts
+++ CopilotKit/packages/runtime-client-gql/src/client/CopilotRuntimeClient.ts
@@ -36,11 +36,15 @@ export class CopilotRuntimeClient {
     });
   }
 
-  generateCopilotResponse(
+  generateCopilotResponse({
+    data,
+    properties,
+    signal,
+  }: {
     data: GenerateCopilotResponseMutationVariables["data"],
     properties?: GenerateCopilotResponseMutationVariables["properties"],
     signal?: AbortSignal,
-  ) {
+  }) {
     const result = this.client.mutation<
       GenerateCopilotResponseMutation,
       GenerateCopilotResponseMutationVariables
--- CopilotKit/packages/runtime/src/graphql/inputs/generate-copilot-response.input.ts
+++ CopilotKit/packages/runtime/src/graphql/inputs/generate-copilot-response.input.ts
@@ -1,10 +1,32 @@
-import { Field, InputType } from "type-graphql";
+import { Field, InputType, registerEnumType } from "type-graphql";
 import { MessageInput } from "./message.input";
 import { FrontendInput } from "./frontend.input";
 import { CloudInput } from "./cloud.input";
 
+export enum CopilotRequestType {
+  Chat = "Chat",
+  Task = "Task",
+  TextareaCompletion = "TextareaCompletion",
+  TextareaPopover = "TextareaPopover",
+  Suggestion = "Suggestion",
+}
+
+registerEnumType(CopilotRequestType, {
+  name: "CopilotRequestType",
+  description: "The type of Copilot request",
+});
+
+@InputType()
+export class GenerateCopilotResponseMetadataInput {
+  @Field(() => String, { nullable: true })
+  requestType: CopilotRequestType;
+}
+
 @InputType()
 export class GenerateCopilotResponseInput {
+  @Field(() => GenerateCopilotResponseMetadataInput, { nullable: false })
+  metadata: GenerateCopilotResponseMetadataInput;
+
   @Field(() => String, { nullable: true })
   threadId?: string;
 
