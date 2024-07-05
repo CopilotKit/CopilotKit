e09b0073b1975d660956b30c799273a19b55c34d
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2024-07-05 13:02:34 +0200

    remove deprecated methods, update default cloud runtime url with `v1` suffix (#429)
    * remove deprecated functions and types
    
    * feat(shared): adjust copilot cloud API url with /v1 version
    
    ---------
    
    Co-authored-by: Markus Ecker <markus.ecker@gmail.com>

--- CopilotKit/packages/react-core/src/hooks/index.ts
+++ CopilotKit/packages/react-core/src/hooks/index.ts
@@ -2,9 +2,7 @@ export { useCopilotChat } from "./use-copilot-chat";
 export type { UseCopilotChatOptions } from "./use-copilot-chat";
 export type { UseCopilotChatReturn } from "./use-copilot-chat";
 
-export { useMakeCopilotActionable } from "./use-make-copilot-actionable";
 export { useCopilotAction } from "./use-copilot-action";
-export { useMakeCopilotReadable } from "./use-make-copilot-readable";
 export { useMakeCopilotDocumentReadable } from "./use-make-copilot-document-readable";
 export { type UseChatHelpers } from "./use-chat";
 export { useCopilotReadable } from "./use-copilot-readable";
