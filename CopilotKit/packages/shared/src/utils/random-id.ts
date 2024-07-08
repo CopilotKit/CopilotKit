afddc41daec807d51aef353b7dff28ce35135900
Markus Ecker <markus.ecker@gmail.com>
2024-07-08 11:57:43 +0200

    switch to uuids

--- CopilotKit/packages/shared/src/utils/random-id.ts
+++ CopilotKit/packages/shared/src/utils/random-id.ts
@@ -1,13 +1,3 @@
-export function randomId(length = 16, alphabet = "abcdefghijklmnopqrstuvwxyz0123456789") {
-  const alphabetLength = alphabet.length;
-  const randomArray = new Uint8Array(length);
-  globalThis.crypto.getRandomValues(randomArray);
-
-  let id = "";
-  for (let i = 0; i < length; i++) {
-    id += alphabet[randomArray[i] % alphabetLength];
-  }
-  return id;
+export function randomId() {
+  return "ck-" + globalThis.crypto.randomUUID();
 }
-
-export default randomId;
