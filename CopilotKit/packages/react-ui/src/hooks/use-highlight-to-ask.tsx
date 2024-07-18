a63fbe1e80877fc1976c7e609b027329b9725465
farhan <farhan@add3.com>
2024-07-19 02:48:04 +0530

    created useHighlightToAsk hook

---
+++ CopilotKit/packages/react-ui/src/hooks/use-highlight-to-ask.tsx
@@ -0,0 +1,125 @@
+import { useCopilotReadable, useCopilotChat } from "@copilotkit/react-core";
+import { useEffect, useRef, useState } from "react";
+import { Role, TextMessage } from "@copilotkit/runtime-client-gql";
+
+export interface UseHighlightToAskConfigurations {
+  /**
+   * The description of the information to be added to the Copilot context.
+   */
+  description: string;
+  /**
+   * The value to be added to the Copilot context.
+   */
+  value: any;
+  /**
+   * The ID of the parent context, if any.
+   */
+  parentId?: string;
+  /**
+   * An array of categories to control which context are visible where. Particularly useful
+   * with CopilotTextarea (see `useMakeAutosuggestionFunction`)
+   */
+  categories?: string[];
+
+  /**
+   * A custom conversion function to use to serialize the value to a string. If not provided, the value
+   * will be serialized using `JSON.stringify`.
+   */
+  convert?: (description: string, value: any) => string;
+}
+
+export function useHighlightToAsk(
+  { description, value, parentId, categories, convert }: UseHighlightToAskConfigurations,
+  dependencies?: any[],
+) {
+  useCopilotReadable({ description, value, parentId, categories, convert }, dependencies);
+
+  const { appendMessage } = useCopilotChat();
+
+  const [tooltipPosition, setTooltipPosition] = useState({ top: 0, left: 0 });
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+  const [highlightedText, setHighlightedText] = useState("");
+
+  const createTooltipElement = () => {
+    if (typeof window !== "undefined") {
+      const tooltip = document.createElement("div");
+      tooltip.style.position = "absolute";
+      tooltip.style.backgroundColor = "#333";
+      tooltip.style.color = "#fff";
+      tooltip.style.padding = "5px";
+      tooltip.style.borderRadius = "5px";
+      tooltip.style.display = "none";
+      tooltip.style.zIndex = "1000";
+      tooltip.style.cursor = "pointer";
+      tooltipRef.current = tooltip;
+      document.body.appendChild(tooltip);
+    }
+  };
+
+  function displayTooltip(text: string) {
+    if (!!tooltipRef.current) {
+      tooltipRef.current.textContent = "Ask AI";
+      tooltipRef.current.style.top = `${tooltipPosition.top}px`;
+      tooltipRef.current.style.left = `${tooltipPosition.left}px`;
+      if (!!text) {
+        tooltipRef.current.onclick = () => {
+          appendMessage(
+            new TextMessage({
+              content: `Hi, I want to understand what '${text}' means here. Thanks :)`,
+              role: Role.User,
+            }),
+          );
+        };
+      }
+      tooltipRef.current.style.display = !!text ? "block" : "none";
+    }
+  }
+
+  function removeHighlightedText() {
+    if (typeof window !== "undefined") {
+      document.getSelection()?.collapseToEnd();
+    }
+    setHighlightedText("");
+  }
+
+  function readHighlightedText() {
+    let text = "";
+    if (typeof window !== "undefined") {
+      text = document.getSelection()?.toString() || "";
+      if (text) {
+        const selection = document.getSelection();
+        const range = selection?.getRangeAt(0).cloneRange();
+        const rect = range?.getBoundingClientRect();
+        if (rect) {
+          setTooltipPosition({
+            top: rect.bottom + window.scrollY,
+            left: rect.right + window.scrollX,
+          });
+        }
+      }
+    }
+    setHighlightedText(text);
+  }
+
+  function listenToHighlightedText() {
+    if (typeof window !== "undefined") document.addEventListener("mouseup", readHighlightedText);
+  }
+
+  useEffect(() => {
+    createTooltipElement();
+    listenToHighlightedText();
+
+    return () => {
+      if (typeof window !== "undefined") {
+        document.removeEventListener("mouseup", removeHighlightedText);
+        if (tooltipRef.current) {
+          document.body.removeChild(tooltipRef.current);
+        }
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    displayTooltip(highlightedText);
+  }, [highlightedText]);
+}
