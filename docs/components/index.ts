eb34ce349c71f272dd25e718effb4b0c1fcca538
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-29 12:44:44 +0200

    feat: add concepts - generative ui

---
+++ docs/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./frame";
+export * from "./sidenav/sidenav-title-with-icon";
+export * from "./reference/property-reference";
+
---
+++ docs/pages/concepts/_meta.tsx
@@ -0,0 +1,17 @@
+import { SideNavTitleWithIcon } from "../../components/sidenav/sidenav-title-with-icon";
+import { FaNodeJs } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { PiGraphDuotone } from "react-icons/pi";
+
+export default {
+  "generative-ui": {
+    title: <SideNavTitleWithIcon title="Generative UI" icon={FaReact} />,
+  },
+  "copilot-runtime": {
+    title: <SideNavTitleWithIcon title="Copilot Runtime" icon={FaNodeJs} />,
+  },
+  "agents": {
+    title: <SideNavTitleWithIcon title="Agents (LangChain)" icon={PiGraphDuotone} />,
+  },
+}
+
