a241f9ba7f08ba268f951c006bd8e7b553f37e10
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-07-29 13:36:35 +0200

    updated copilot runtime docs

--- docs/components/frame.tsx
+++ docs/components/frame.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
-export function Frame({ children, className }: { children: React.ReactNode, className: string }) {
+export function Frame({
+  children,
+  className,
+  description,
+}: {
+  children: React.ReactNode;
+  className: string;
+  description?: string;
+}) {
   return (
-  <div className={`flex space-x-4 w-full mx-auto justify-center my-4 ${className}`}>
-    {React.Children.map(children, (child) =>
-      React.isValidElement(child) ? React.cloneElement(child as React.ReactElement<any>, {
-        className: `border border-neutral-200 rounded-md shadow-lg ${child.props.className || ""}`
-      }) : child
-    )}
-  </div>
+    <>
+      <div
+        className={`flex space-x-4 w-full mx-auto justify-center my-4 bg-white ${className}`}
+      >
+        {React.Children.map(children, (child) =>
+          React.isValidElement(child)
+            ? React.cloneElement(child as React.ReactElement<any>, {
+                className: `border border-neutral-200 rounded-md shadow-lg ${
+                  child.props.className || ""
+                }`,
+              })
+            : child
+        )}
+      </div>
+      {description && <p className="text-sm text-neutral-500 text-center">{description}</p>}
+    </>
   );
 }
