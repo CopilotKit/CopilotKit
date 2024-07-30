1c5e9fb96594f91eb1ff82cf3222894ff82f3b21
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2024-07-30 20:25:01 +0200

    feat(docs): analytics (#516)
    * feat(docs): add rb2b
    
    * feat(docs): posthog
    
    * feat(docs): add scarf pixel
    
    * remove other duplicates of scarf pixel

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -7,6 +7,12 @@ const withNextra = nextra({
 })
 
 export default withNextra({
+  env: {
+    RB2B_ID: process.env.RB2B_ID,
+    POSTHOG_KEY: process.env.POSTHOG_KEY,
+    POSTHOG_HOST: process.env.POSTHOG_HOST,
+    SCARF_PIXEL_ID: process.env.SCARF_PIXEL_ID,
+  },
   async redirects() {
     return [
       {
