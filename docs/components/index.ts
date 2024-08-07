b9ad00dc51b73676de14051f53d2ea07aec6d56f
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-08-07 17:15:21 +0200

    remove accordion component

--- docs/components/index.ts
+++ docs/components/index.ts
@@ -4,4 +4,3 @@ export * from "./reference/property-reference";
 export * from "./copilotkit-css";
 export * from "./link-to-copilot-cloud";
 export * from "./copilot-cloud-or-self-hosting";
-export * from "./accordion";
