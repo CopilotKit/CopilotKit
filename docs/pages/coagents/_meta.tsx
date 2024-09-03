b5733f3d51c3c81d044da94031344b2e12fc0f60
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-03 14:40:39 +0200

    hidden coagents route

---
+++ docs/pages/coagents/_meta.tsx
@@ -0,0 +1,21 @@
+export default {
+  "___coagents": {
+    "type": "separator",
+    "title": "CoAgents (Alpha)"
+  },
+  "index": {
+    title: "Getting Started With CoAgents",
+  },
+  "feature-shared-state": {
+    title: "Feature: Shared State",
+  },
+  "feature-state-streaming": {
+    title: "Feature: State Streaming",
+  },
+  "feature-agent-qna": {
+    title: "Feature: Agent Q&A",
+  },
+  "feature-agent-steering": {
+    title: "Feature: Agent Steering (Coming Soon)",
+  }
+}
