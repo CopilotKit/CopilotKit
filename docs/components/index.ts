27ce4af9034cf0419ebdc19b9b10fa6965b5b722
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-05 13:03:06 +0200

    fix import

--- docs/components/index.ts
+++ docs/components/index.ts
@@ -5,3 +5,4 @@ export * from "./copilotkit-css";
 export * from "./link-to-copilot-cloud";
 export * from "./copilot-cloud-or-self-hosting";
 export * from "./youtube-video";
+export * from "./coagents-enterprise-cta";
