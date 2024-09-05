fb08de5e25ab7b662bd868a77b4fa26c84e47519
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-05 12:49:54 +0200

    update docs

--- docs/pages/coagents/_meta.tsx
+++ docs/pages/coagents/_meta.tsx
@@ -1,7 +1,14 @@
 export default {
-  ___coagents: {
-    type: "separator",
-    title: "CoAgents (Alpha)",
+  "___coagents": {
+    "type": "separator",
+    "title": () => (
+      <div className="flex items-center gap-2">
+        <span>CoAgents</span>
+        <div className="text-indigo-600 border border-indigo-600 px-1 py-1 rounded-md font-medium" style={{ fontSize: "0.6rem" }}>
+          ALPHA
+        </div>
+      </div>
+    )
   },
   index: {
     title: "Introduction to CoAgents",
