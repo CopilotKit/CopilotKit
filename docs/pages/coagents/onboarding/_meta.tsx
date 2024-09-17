286a0517f8da2878e20639d229db3a02a30f3b41
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-17 18:48:54 +0200

    add onboarding route

---
+++ docs/pages/coagents/onboarding/_meta.tsx
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: "CoAgents Onboarding",
+    theme: {
+      sidebar: false,
+      footer: false,
+      breadcrumb: false,
+      pagination: false,
+    },
+  },
+};
