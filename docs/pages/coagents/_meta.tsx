1217148e26e4707aae7b352c82fdcedb683d0fbe
David Demaree <david@demaree.me>
2024-09-20 14:23:56 -0400

    Restored old getting started content with edits

--- docs/pages/coagents/_meta.tsx
+++ docs/pages/coagents/_meta.tsx
@@ -2,6 +2,9 @@ export default {
   index: {
     title: "Introduction to CoAgents",
   },
+  "getting-started": {
+    title: "Getting Started",
+  },
   "advanced-usage": {
     title: "Advanced Usage",
   },
