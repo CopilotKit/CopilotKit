adaf5664a6a426a4835c07ec43fcbc4ca043d1e2
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-09-23 13:49:11 +0200

    add preserve directives plugin

--- CopilotKit/packages/react-ui/vite.config.ts
+++ CopilotKit/packages/react-ui/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import preserveDirectives from "rollup-preserve-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), libInjectCss(), dts({ include: "src" })],
+  plugins: [react(), libInjectCss(), dts({ include: "src" }), preserveDirectives()],
   build: {
     outDir: "dist",
     rollupOptions: {
