feada10a8f9828501fc293e357a2c2138355f5bb
Markus Ecker <markus.ecker@gmail.com>
2024-09-27 13:30:21 +0200

    implement renderAndWait

--- CopilotKit/packages/react-core/src/types/frontend-action.ts
+++ CopilotKit/packages/react-core/src/types/frontend-action.ts
@@ -37,6 +37,48 @@ interface CompleteStateNoArgs<T extends Parameter[] | [] = []> {
   result: any;
 }
 
+interface InProgressStateWait<T extends Parameter[] | [] = []> {
+  status: "inProgress";
+  args: Partial<MappedParameterTypes<T>>;
+  handler: undefined;
+  result: undefined;
+}
+
+interface ExecutingStateWait<T extends Parameter[] | [] = []> {
+  status: "executing";
+  args: MappedParameterTypes<T>;
+  handler: (result: any) => void;
+  result: undefined;
+}
+
+interface CompleteStateWait<T extends Parameter[] | [] = []> {
+  status: "complete";
+  args: MappedParameterTypes<T>;
+  handler: undefined;
+  result: any;
+}
+
+interface InProgressStateNoArgsWait<T extends Parameter[] | [] = []> {
+  status: "inProgress";
+  args: Partial<MappedParameterTypes<T>>;
+  handler: undefined;
+  result: undefined;
+}
+
+interface ExecutingStateNoArgsWait<T extends Parameter[] | [] = []> {
+  status: "executing";
+  args: MappedParameterTypes<T>;
+  handler: (result: any) => void;
+  result: undefined;
+}
+
+interface CompleteStateNoArgsWait<T extends Parameter[] | [] = []> {
+  status: "complete";
+  args: MappedParameterTypes<T>;
+  handler: undefined;
+  result: any;
+}
+
 export type ActionRenderProps<T extends Parameter[] | [] = []> =
   | CompleteState<T>
   | ExecutingState<T>
@@ -47,13 +89,34 @@ export type ActionRenderPropsNoArgs<T extends Parameter[] | [] = []> =
   | ExecutingStateNoArgs<T>
   | InProgressStateNoArgs<T>;
 
+export type ActionRenderPropsWait<T extends Parameter[] | [] = []> =
+  | CompleteStateWait<T>
+  | ExecutingStateWait<T>
+  | InProgressStateWait<T>;
+
+export type ActionRenderPropsNoArgsWait<T extends Parameter[] | [] = []> =
+  | CompleteStateNoArgsWait<T>
+  | ExecutingStateNoArgsWait<T>
+  | InProgressStateNoArgsWait<T>;
+
 export type FrontendAction<T extends Parameter[] | [] = []> = Action<T> & {
   disabled?: boolean;
-  render?:
-    | string
-    | (T extends []
-        ? (props: ActionRenderPropsNoArgs<T>) => string | React.ReactElement
-        : (props: ActionRenderProps<T>) => string | React.ReactElement);
-};
+} & (
+    | {
+        render?:
+          | string
+          | (T extends []
+              ? (props: ActionRenderPropsNoArgs<T>) => string | React.ReactElement
+              : (props: ActionRenderProps<T>) => string | React.ReactElement);
+        renderAndWait?: never;
+      }
+    | {
+        render?: never;
+        renderAndWait: T extends []
+          ? (props: ActionRenderPropsNoArgsWait<T>) => React.ReactElement
+          : (props: ActionRenderPropsWait<T>) => React.ReactElement;
+        handler?: never;
+      }
+  );
 
 export type RenderFunctionStatus = ActionRenderProps<any>["status"];
