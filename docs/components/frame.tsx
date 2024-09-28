d79ea5c746eb2d40c35bcce0f22fd2c16484f44a
Chandravan <100784095+Chandravan@users.noreply.github.com>
2024-09-29 01:30:06 +0530

    Update frame.tsx
    Prefix the added class with a unique identifier to avoid conflicts

--- docs/components/frame.tsx
+++ docs/components/frame.tsx
@@ -13,13 +13,13 @@ export function Frame({
     <>
       <div
         className={`flex space-x-4 w-full mx-auto justify-center my-4 ${className}`}
+      
       >
         {React.Children.map(children, (child) =>
           React.isValidElement(child)
             ? React.cloneElement(child as React.ReactElement<any>, {
-                className: `border border-neutral-200 rounded-md shadow-lg bg-white ${
-                  child.props.className || ""
-                }`,
+              
+              className: `frame-child border border-neutral-200 rounded-md shadow-lg bg-white ${child.props.className || ""}`,
               })
             : child
         )}
