086e71766fbbb2d6b010e561a2f386c72f87483a
Markus Ecker <markus.ecker@gmail.com>
2024-10-04 10:10:15 +0200

    update shared state agent

--- examples/coagents-shared-state/agent/my_agent/agent.py
+++ examples/coagents-shared-state/agent/my_agent/agent.py
@@ -4,15 +4,34 @@ It defines the workflow graph and the entry point for the agent.
 """
 # pylint: disable=line-too-long, unused-import
 
-import json
+import os
 from typing import cast, TypedDict
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import SystemMessage, ToolMessage, AIMessage, HumanMessage
+from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import ToolMessage, AIMessage, HumanMessage, SystemMessage
 from langchain_core.runnables import RunnableConfig
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import MessagesState
 from copilotkit.langchain import copilotkit_customize_config
+
+def get_model():
+    """
+    Get a model based on the environment variable.
+    """
+    model = os.getenv("MODEL", "openai")
+
+    if model == "openai":
+        return ChatOpenAI(temperature=0, model="gpt-4o")
+    if model == "anthropic":
+        return ChatAnthropic(
+            temperature=0,
+            model_name="claude-3-5-sonnet-20240620",
+            timeout=None,
+            stop=None
+        )
+
+    raise ValueError("Invalid model specified")
 
 class Translations(TypedDict):
     """Contains the translations in four different languages."""
@@ -34,14 +53,13 @@ async def translate_node(state: AgentState, config: RunnableConfig):
         emit_intermediate_state=[
             {
                 "state_key": "translations",
-                "tool": "translate"
+                "tool": "Translations"
             }
         ]
     )
 
-    model = ChatOpenAI(model="gpt-4o").bind_tools(
+    model = get_model().bind_tools(
         [Translations],
-        parallel_tool_calls=False,
         tool_choice=(
             None if state["messages"] and
             isinstance(state["messages"][-1], HumanMessage)
@@ -49,25 +67,38 @@ async def translate_node(state: AgentState, config: RunnableConfig):
         )
     )
 
-    response = await model.ainvoke([
-        SystemMessage(
-            content=f"""
-            You are a helpful assistant that translates text to different languages 
-            (Spanish, French and German).
-            Don't ask for confirmation before translating.
-            {
-                'The user is currently working on translating this text: "' + 
-                state["input"] + '"' if state.get("input") else ""
-            }
-            """
-        ),
-        *state["messages"],
-    ], config)
+    new_message = HumanMessage(
+        content=f"""
+        You are a helpful assistant that translates text to different languages 
+        (Spanish, French and German).
+        Don't ask for confirmation before translating.
+        {
+            'The user is currently working on translating this text: "' + 
+            state["input"] + '"' if state.get("input") else ""
+        }
+        """
+    )
+
+    # remove system message and empty AI messages
+    messages = [
+        message for message in state["messages"]
+        if not isinstance(message, SystemMessage) and 
+        not (isinstance(message, AIMessage) and message.content == "")
+    ]
+    messages += [new_message]
+
+    for message in messages:
+        print(message)
+        print(type(message))
+        print("---")
+
+    response = await model.ainvoke(messages, config)
 
     if hasattr(response, "tool_calls") and len(getattr(response, "tool_calls")) > 0:
         ai_message = cast(AIMessage, response)
         return {
             "messages": [
+                new_message,
                 response,
                 ToolMessage(
                     content="Translated!",
@@ -78,7 +109,8 @@ async def translate_node(state: AgentState, config: RunnableConfig):
         }
 
     return {
-        "messages": [           
+        "messages": [
+            new_message,
             response,
         ],
     }
