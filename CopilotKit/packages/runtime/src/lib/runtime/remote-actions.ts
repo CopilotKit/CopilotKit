29250070642374a7581be936c6b1a77ab9af3a11
Ran Shem Tov <ran@copilotkit.ai>
2024-10-17 19:17:00 +0200

    feat(remote-actions): integrate langgraph-cloud as a remote action for CoAgents

--- CopilotKit/packages/runtime/src/lib/runtime/remote-actions.ts
+++ CopilotKit/packages/runtime/src/lib/runtime/remote-actions.ts
@@ -1,14 +1,16 @@
 import { Action } from "@copilotkit/shared";
 import { GraphQLContext } from "../integrations/shared";
 import { Logger } from "pino";
-import telemetry from "../../lib/telemetry-client";
 import { Message } from "../../graphql/types/converted";
 import { RuntimeEvent } from "../../service-adapters/events";
-import { RemoteLangGraphEventSource } from "../../agents/langgraph/event-source";
-import { Observable, ReplaySubject } from "rxjs";
+import { Observable } from "rxjs";
 import { ActionInput } from "../../graphql/inputs/action.input";
 import { AgentStateInput } from "../../graphql/inputs/agent-state.input";
-import { LangGraphEvent } from "../../agents/langgraph/events";
+import {
+  constructLGCRemoteAction,
+  constructRemoteActions,
+  createHeaders,
+} from "./remote-action-constructors";
 
 export type RemoteEndpointDefinition = RemoteAction | RemoteLangGraphCloudAction;
 
@@ -64,24 +66,6 @@ export function isLangGraphAgentAction(action: Action<any>): action is LangGraph
   return typeof (action as LangGraphAgentAction).langGraphAgentHandler === "function";
 }
 
-function createHeaders(
-  onBeforeRequest: RemoteAction["onBeforeRequest"],
-  graphqlContext: GraphQLContext,
-) {
-  const headers = {
-    "Content-Type": "application/json",
-  };
-
-  if (onBeforeRequest) {
-    const { headers: additionalHeaders } = onBeforeRequest({ ctx: graphqlContext });
-    if (additionalHeaders) {
-      Object.assign(headers, additionalHeaders);
-    }
-  }
-
-  return headers;
-}
-
 async function fetchRemoteInfo({
   url,
   onBeforeRequest,
@@ -125,182 +109,6 @@ async function fetchRemoteInfo({
   }
 }
 
-function constructRemoteActions({
-  json,
-  url,
-  onBeforeRequest,
-  graphqlContext,
-  logger,
-  messages,
-  agentStates,
-}: {
-  json: RemoteActionInfoResponse;
-  url: string;
-  onBeforeRequest?: RemoteAction["onBeforeRequest"];
-  graphqlContext: GraphQLContext;
-  logger: Logger;
-  messages: Message[];
-  agentStates?: AgentStateInput[];
-}): Action<any>[] {
-  const actions = json["actions"].map((action) => ({
-    name: action.name,
-    description: action.description,
-    parameters: action.parameters,
-    handler: async (args: any) => {
-      logger.debug({ actionName: action.name, args }, "Executing remote action");
-
-      const headers = createHeaders(onBeforeRequest, graphqlContext);
-      telemetry.capture("oss.runtime.remote_action_executed", {});
-
-      try {
-        const response = await fetch(`${url}/actions/execute`, {
-          method: "POST",
-          headers,
-          body: JSON.stringify({
-            name: action.name,
-            arguments: args,
-            properties: graphqlContext.properties,
-          }),
-        });
-
-        if (!response.ok) {
-          logger.error(
-            { url, status: response.status, body: await response.text() },
-            "Failed to execute remote action",
-          );
-          return "Failed to execute remote action";
-        }
-
-        const requestResult = await response.json();
-
-        const result = requestResult["result"];
-        logger.debug({ actionName: action.name, result }, "Executed remote action");
-        return result;
-      } catch (error) {
-        logger.error(
-          { error: error.message ? error.message : error + "" },
-          "Failed to execute remote action",
-        );
-        return "Failed to execute remote action";
-      }
-    },
-  }));
-
-  const agents = json["agents"].map((agent) => ({
-    name: agent.name,
-    description: agent.description,
-    parameters: [],
-    handler: async (_args: any) => {},
-
-    langGraphAgentHandler: async ({
-      name,
-      actionInputsWithoutAgents,
-      threadId,
-      nodeName,
-    }: LangGraphAgentHandlerParams): Promise<Observable<RuntimeEvent>> => {
-      logger.debug({ actionName: agent.name }, "Executing remote agent");
-
-      const headers = createHeaders(onBeforeRequest, graphqlContext);
-      telemetry.capture("oss.runtime.remote_action_executed", {});
-
-      let state = {};
-      if (agentStates) {
-        const jsonState = agentStates.find((state) => state.agentName === name)?.state;
-        if (jsonState) {
-          state = JSON.parse(jsonState);
-        }
-      }
-
-      const response = await fetch(`${url}/agents/execute`, {
-        method: "POST",
-        headers,
-        body: JSON.stringify({
-          name,
-          threadId,
-          nodeName,
-          messages,
-          state,
-          properties: graphqlContext.properties,
-          actions: actionInputsWithoutAgents.map((action) => ({
-            name: action.name,
-            description: action.description,
-            parameters: JSON.parse(action.jsonSchema),
-          })),
-        }),
-      });
-
-      if (!response.ok) {
-        logger.error(
-          { url, status: response.status, body: await response.text() },
-          "Failed to execute remote agent",
-        );
-        throw new Error("Failed to execute remote agent");
-      }
-
-      const eventSource = new RemoteLangGraphEventSource();
-      streamResponse(response, eventSource.eventStream$);
-      return eventSource.processLangGraphEvents();
-    },
-  }));
-
-  return [...actions, ...agents];
-}
-
-async function streamResponse(response: Response, eventStream$: ReplaySubject<LangGraphEvent>) {
-  const reader = response.body!.getReader();
-  const decoder = new TextDecoder();
-  let buffer = [];
-
-  function flushBuffer() {
-    const currentBuffer = buffer.join("");
-    if (currentBuffer.trim().length === 0) {
-      return;
-    }
-    const parts = currentBuffer.split("\n");
-    if (parts.length === 0) {
-      return;
-    }
-
-    const lastPartIsComplete = currentBuffer.endsWith("\n");
-
-    // truncate buffer
-    buffer = [];
-
-    if (!lastPartIsComplete) {
-      // put back the last part
-      buffer.push(parts.pop());
-    }
-
-    parts
-      .map((part) => part.trim())
-      .filter((part) => part != "")
-      .forEach((part) => {
-        eventStream$.next(JSON.parse(part));
-      });
-  }
-
-  try {
-    while (true) {
-      const { done, value } = await reader.read();
-
-      if (!done) {
-        buffer.push(decoder.decode(value, { stream: true }));
-      }
-
-      flushBuffer();
-
-      if (done) {
-        break;
-      }
-    }
-  } catch (error) {
-    console.error("Error in stream", error);
-    eventStream$.error(error);
-    return;
-  }
-  eventStream$.complete();
-}
-
 export async function setupRemoteActions({
   remoteEndpointDefinitions,
   graphqlContext,
@@ -329,8 +137,16 @@ export async function setupRemoteActions({
     filtered.map(async (actionDefinition) => {
       // Check for properties that can distinguish LG cloud from other actions
       if (actionDefinition.type === RemoteEndpointType.LangGraphCloud) {
-        // TODO: Construct LG Cloud remote action
-        return;
+        return constructLGCRemoteAction({
+          action: actionDefinition,
+          messages,
+          graphqlContext,
+          logger: logger.child({
+            component: "remote-actions.constructLGCRemoteAction",
+            actionDefinition,
+          }),
+          agentStates,
+        });
       }
 
       const json = await fetchRemoteInfo({
---
+++ CopilotKit/packages/runtime/src/lib/runtime/remote-lg-cloud-action.ts
@@ -0,0 +1,371 @@
+import { Client } from "@langchain/langgraph-sdk";
+import { randomUUID } from "node:crypto";
+import { parse as parsePartialJson } from "partial-json";
+import { ActionInput } from "../../graphql/inputs/action.input";
+import { RemoteLangGraphAgent } from "./remote-actions";
+import { CopilotRequestContextProperties } from "../integrations";
+import { BaseMessage as CopilotKitBaseMessage } from "../../graphql/types/base";
+
+type State = Record<string, any>;
+
+type ExecutionAction = Pick<ActionInput, "name" | "description"> & { parameters: string };
+
+interface ExecutionArgs {
+  agent: RemoteLangGraphAgent;
+  threadId: string;
+  nodeName: string;
+  messages: CopilotKitBaseMessage[];
+  state: State;
+  properties: CopilotRequestContextProperties;
+  actions: ExecutionAction[];
+}
+
+export async function execute(args: ExecutionArgs): Promise<ReadableStream<Uint8Array>> {
+  return new ReadableStream({
+    async start(controller) {
+      try {
+        await streamEvents(controller, args);
+        controller.close();
+      } catch (err) {}
+    },
+  });
+}
+
+async function streamEvents(controller: ReadableStreamDefaultController, args: ExecutionArgs) {
+  const {
+    threadId: agrsInitialThreadId,
+    agent,
+    nodeName: initialNodeName,
+    state: initialState,
+    messages,
+    actions,
+  } = args;
+
+  let nodeName = initialNodeName;
+  let state = initialState;
+  const { name, assistantId: initialAssistantId } = agent;
+
+  const client = new Client();
+  let initialThreadId = agrsInitialThreadId;
+  const wasInitiatedWithExistingThread = !!initialThreadId;
+  if (initialThreadId && initialThreadId.startsWith("ck-")) {
+    initialThreadId = initialThreadId.substring(3);
+  }
+
+  const assistants = await client.assistants.search();
+  const retrievedAssistant = assistants.find((a) => a.name === name);
+  const threadId = initialThreadId ?? randomUUID();
+  await client.threads.create({ threadId: threadId });
+
+  let agentState = { values: {} };
+  if (wasInitiatedWithExistingThread) {
+    agentState = await client.threads.getState(threadId);
+  }
+  const agentStateValues = agentState.values as State;
+  state.messages = agentStateValues.messages;
+  const mode = wasInitiatedWithExistingThread && nodeName != "__end__" ? "continue" : "start";
+  state = langGraphDefaultMergeState(state, messages, actions);
+
+  if (mode === "continue") {
+    await client.threads.updateState(threadId, { values: state, asNode: nodeName });
+  }
+
+  const assistantId = initialAssistantId ?? retrievedAssistant.assistant_id;
+  const graphInfo = await client.assistants.getGraph(assistantId);
+  const streamInput = mode === "start" ? state : null;
+
+  let streamingStateExtractor = new StreamingStateExtractor([]);
+  let prevNodeName = null;
+  let emitIntermediateStateUntilEnd = null;
+  let shouldExit = null;
+  let externalRunId = null;
+
+  const streamResponse = client.runs.stream(threadId, assistantId, {
+    input: streamInput,
+    streamMode: ["events", "values"],
+  });
+
+  const emit = (message: string) => controller.enqueue(new TextEncoder().encode(message));
+
+  let latestStateValues = {};
+
+  for await (const chunk of streamResponse) {
+    if (!["events", "values"].includes(chunk.event)) continue;
+
+    if (chunk.event === "values") {
+      latestStateValues = chunk.data;
+      continue;
+    }
+
+    const event = chunk.data;
+    const currentNodeName = event.name;
+    const eventType = event.event;
+    const runId = event.metadata.run_id;
+    externalRunId = runId;
+    const tags = event.tags;
+    const metadata = event.metadata;
+
+    shouldExit = shouldExit != null ? shouldExit : tags.includes("copilotkit:exit");
+    const emitIntermediateState = metadata["copilotkit:emit-intermediate-state"];
+    const forceEmitIntermediateState = tags.includes("copilotkit:force-emit-intermediate-state");
+    // we only want to update the node name under certain conditions
+    // since we don't need any internal node names to be sent to the frontend
+    if (graphInfo["nodes"].some((node) => node.id === currentNodeName)) {
+      nodeName = currentNodeName;
+    }
+
+    if (!nodeName) {
+      continue;
+    }
+
+    if (forceEmitIntermediateState) {
+      if (eventType === "on_chain_end") {
+        state = event.data.output;
+        emit(
+          getStateSyncEvent({
+            threadId,
+            runId,
+            agentName: agent.name,
+            nodeName,
+            state: event.data.output,
+            running: true,
+            active: true,
+          }),
+        );
+      }
+      continue;
+    }
+
+    if (emitIntermediateState && emitIntermediateStateUntilEnd == null) {
+      emitIntermediateStateUntilEnd = nodeName;
+    }
+
+    if (emitIntermediateState && eventType === "on_chat_model_start") {
+      // reset the streaming state extractor
+      streamingStateExtractor = new StreamingStateExtractor(emitIntermediateState);
+    }
+
+    let updatedState = latestStateValues;
+
+    if (emitIntermediateState && eventType === "on_chat_model_stream") {
+      streamingStateExtractor.bufferToolCalls(event);
+    }
+
+    if (emitIntermediateStateUntilEnd !== null) {
+      updatedState = {
+        ...updatedState,
+        ...streamingStateExtractor.extractState(),
+      };
+    }
+
+    if (
+      !emitIntermediateState &&
+      currentNodeName === emitIntermediateStateUntilEnd &&
+      eventType === "on_chain_end"
+    ) {
+      // stop emitting function call state
+      emitIntermediateStateUntilEnd = null;
+    }
+
+    const exitingNode = nodeName === currentNodeName && eventType === "on_chain_end";
+
+    if (
+      JSON.stringify(updatedState) !== JSON.stringify(state) ||
+      prevNodeName != nodeName ||
+      exitingNode
+    ) {
+      state = updatedState;
+      prevNodeName = nodeName;
+      emit(
+        getStateSyncEvent({
+          threadId,
+          runId,
+          agentName: agent.name,
+          nodeName,
+          state,
+          running: true,
+          active: !exitingNode,
+        }),
+      );
+    }
+
+    emit(JSON.stringify(event) + "\n");
+  }
+
+  state = await client.threads.getState(threadId);
+  const isEndNode = state.next.length === 0;
+  nodeName = Object.keys(state.metadata.writes)[0];
+
+  emit(
+    getStateSyncEvent({
+      threadId,
+      runId: externalRunId,
+      agentName: agent.name,
+      nodeName: isEndNode ? "__end__" : nodeName,
+      state: state.values,
+      running: !shouldExit,
+      active: false,
+    }),
+  );
+
+  return Promise.resolve();
+}
+
+function getStateSyncEvent({
+  threadId,
+  runId,
+  agentName,
+  nodeName,
+  state,
+  running,
+  active,
+}: {
+  threadId: string;
+  runId: string;
+  agentName: string;
+  nodeName: string;
+  state: State;
+  running: boolean;
+  active: boolean;
+}): string {
+  const stateWithoutMessages = Object.keys(state).reduce((acc, key) => {
+    if (key !== "messages") {
+      acc[key] = state[key];
+    }
+    return acc;
+  }, {} as State);
+
+  return (
+    JSON.stringify({
+      event: "on_copilotkit_state_sync",
+      thread_id: threadId,
+      run_id: runId,
+      agent_name: agentName,
+      node_name: nodeName,
+      active: active,
+      state: stateWithoutMessages,
+      running: running,
+      role: "assistant",
+    }) + "\n"
+  );
+}
+
+class StreamingStateExtractor {
+  private emitIntermediateState: { [key: string]: any }[];
+  private toolCallBuffer: { [key: string]: string };
+  private currentToolCall: string | null;
+  private previouslyParsableState: { [key: string]: any };
+
+  constructor(emitIntermediateState: { [key: string]: any }[]) {
+    this.emitIntermediateState = emitIntermediateState;
+    this.toolCallBuffer = {};
+    this.currentToolCall = null;
+    this.previouslyParsableState = {};
+  }
+
+  bufferToolCalls(event: {
+    data: { chunk: { tool_call_chunks: { name: string | null; args: string }[] } };
+  }) {
+    if (event.data.chunk.tool_call_chunks.length > 0) {
+      const chunk = event.data.chunk.tool_call_chunks[0];
+
+      if (chunk.name !== null) {
+        this.currentToolCall = chunk.name;
+        this.toolCallBuffer[this.currentToolCall] = chunk.args;
+      } else if (this.currentToolCall !== null) {
+        this.toolCallBuffer[this.currentToolCall] += chunk.args;
+      }
+    }
+  }
+
+  getEmitStateConfig(currentToolName: string): [string | null, string | null] {
+    for (const config of this.emitIntermediateState) {
+      const stateKey = config["state_key"];
+      const tool = config["tool"];
+      const toolArgument = config["tool_argument"];
+
+      if (currentToolName === tool) {
+        return [toolArgument, stateKey];
+      }
+    }
+    return [null, null];
+  }
+
+  extractState(): State {
+    const state: State = {};
+
+    for (const [key, value] of Object.entries(this.toolCallBuffer)) {
+      const [argumentName, stateKey] = this.getEmitStateConfig(key);
+
+      if (stateKey === null) {
+        continue;
+      }
+
+      let parsedValue;
+      try {
+        parsedValue = parsePartialJson(value);
+      } catch (error) {
+        if (key in this.previouslyParsableState) {
+          parsedValue = this.previouslyParsableState[key];
+        } else {
+          continue;
+        }
+      }
+
+      this.previouslyParsableState[key] = parsedValue;
+
+      if (!argumentName) {
+        state[stateKey] = parsedValue;
+      } else {
+        state[stateKey] = parsedValue[argumentName];
+      }
+    }
+
+    return state;
+  }
+}
+
+// Start of Selection
+function langGraphDefaultMergeState(
+  state: State,
+  messages: CopilotKitBaseMessage[],
+  actions: ExecutionAction[],
+): State {
+  if (messages.length > 0 && "role" in messages[0] && messages[0].role === "system") {
+    // remove system message
+    messages = messages.slice(1);
+  }
+
+  // merge with existing messages
+  const mergedMessages = state.messages || [];
+  const existingMessageIds = new Set(mergedMessages.map((message) => message.id));
+
+  for (const message of messages) {
+    if (!existingMessageIds.has(message.id)) {
+      mergedMessages.push(message);
+    }
+  }
+
+  return deepMerge(state, {
+    messages: mergedMessages,
+    copilotkit: {
+      actions,
+    },
+  });
+}
+
+function deepMerge(obj1: State, obj2: State) {
+  let result = { ...obj1 };
+  for (let key in obj2) {
+    if (typeof obj2[key] === "object" && !Array.isArray(obj2[key])) {
+      if (obj1[key]) {
+        result[key] = deepMerge(obj1[key], obj2[key]);
+      } else {
+        result[key] = { ...obj2[key] };
+      }
+    } else {
+      result[key] = obj2[key];
+    }
+  }
+  return result;
+}
