11afeb24be26c97e290b93c3386158d90aa08346
Atai Barkai <atai.barkai@gmail.com>
2024-10-21 00:11:14 -0700

    arg of email tool call is called email_draft for clarity

--- examples/coagents-qa/ui/app/page.tsx
+++ examples/coagents-qa/ui/app/page.tsx
@@ -7,7 +7,7 @@ import "@copilotkit/react-ui/styles.css";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <CopilotKit runtimeUrl="/api/copilotkit">
+      <CopilotKit runtimeUrl="/api/copilotkit" agent="email_agent">
         <Mailer />
       </CopilotKit>
     </main>
