774149b1c2cb157bbc94e20f3d832c998583be54
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2024-10-22 11:39:09 +0200

    feat(example-next-openai): llm selector (#771)
    * feat(example-next-openai): llm selector
    
    * feat(example-next-opena): add groq to example
    
    * feat(example-next-openai): disable azure
    
    * fix: formatting
    
    * feat: dynamic service adapter selector
    
    * fix: formatting

--- CopilotKit/examples/next-openai/src/app/page.tsx
+++ CopilotKit/examples/next-openai/src/app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { CopilotKit } from "@copilotkit/react-core";
 import { CopilotSidebar } from "@copilotkit/react-ui";
 import { VacationList } from "./components/vacation-list";
+import { useSearchParams } from "next/navigation";
 
 export default function WaterBnb() {
+  const searchParams = useSearchParams();
+  const serviceAdapter = searchParams.get("serviceAdapter") || "openai";
+  const runtimeUrl =
+    process.env["NEXT_PUBLIC_COPILOTKIT_RUNTIME_URL"] ??
+    `/api/copilotkit?serviceAdapter=${serviceAdapter}`;
+
+  const copilotKitProps = {
+    runtimeUrl,
+    publicApiKey: process.env["NEXT_PUBLIC_COPILOTKIT_PUBLIC_API_KEY"] ?? undefined,
+  };
+
   return (
-    <CopilotKit runtimeUrl="/api/copilotkit">
+    <CopilotKit {...copilotKitProps}>
       <CopilotSidebar>
         <VacationList />
       </CopilotSidebar>
