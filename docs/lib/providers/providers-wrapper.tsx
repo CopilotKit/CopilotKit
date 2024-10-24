e7259df3a11ae5c72a196b069a3a0351e6585489
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2024-10-24 17:19:57 +0200

    fix(docs): analytics (#809)
    * fix(docs): analytics
    
    * feat(docs): add Google Analytics
    
    * fix suspense

--- docs/lib/providers/providers-wrapper.tsx
+++ docs/lib/providers/providers-wrapper.tsx
@@ -5,23 +5,26 @@ import { PostHogProvider } from "@/lib/providers/posthog-provider";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ScarfPixel } from "./scarf-pixel";
 import { useRB2B } from "@/lib/hooks/use-rb2b";
+import { useGoogleAnalytics } from "../hooks/use-google-analytics";
 
-export function ProvidersWrapper({
-  children,
-  clerkPublishableKey,
-}: {
-  children: React.ReactNode;
-  clerkPublishableKey: string;
-}) {
+export function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   useRB2B();
+  useGoogleAnalytics();
 
-  return (
-    <>
-      {/* <ClerkProvider publishableKey={clerkPublishableKey}> */}
-        {/* <PostHogProvider>{children}</PostHogProvider> */}
-        {children}
-        <ScarfPixel />
-      {/* </ClerkProvider> */}
-    </>
+  const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+  const toRender = (
+    <Suspense fallback={null}>
+      <PostHogProvider>{children}</PostHogProvider>
+      <ScarfPixel />
+    </Suspense>
   );
+
+  if (clerkPublishableKey) {
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      {toRender}
+    </ClerkProvider>;
+  } else {
+    return toRender;
+  }
 }
--- docs/lib/providers/scarf-pixel.tsx
+++ docs/lib/providers/scarf-pixel.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 
 export function ScarfPixel() {
-  const SCARF_PIXEL_ID = process.env.SCARF_PIXEL_ID;
+  const SCARF_PIXEL_ID = process.env.NEXT_PUBLIC_SCARF_PIXEL_ID;
   if (!SCARF_PIXEL_ID) return null;
   return <img referrerPolicy="no-referrer-when-downgrade" src={`https://static.scarf.sh/a.png?x-pxid=${SCARF_PIXEL_ID}`} />;
 }
