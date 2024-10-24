7cd9e70a4353cf3e0641236cbcf1528a4a51e9dc
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-10-24 17:06:36 +0200

    feat(docs): add Google Analytics

--- docs/lib/providers/providers-wrapper.tsx
+++ docs/lib/providers/providers-wrapper.tsx
@@ -5,9 +5,11 @@ import { PostHogProvider } from "@/lib/providers/posthog-provider";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ScarfPixel } from "./scarf-pixel";
 import { useRB2B } from "@/lib/hooks/use-rb2b";
+import { useGoogleAnalytics } from "../hooks/use-google-analytics";
 
 export function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   useRB2B();
+  useGoogleAnalytics();
 
   const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
