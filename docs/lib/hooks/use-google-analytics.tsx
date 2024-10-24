0410f80be0eccdc4f0e5d9b58ebcfb7b65eb1b88
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-10-24 17:48:13 +0200

    fix(docs): ga tracking id

--- docs/lib/hooks/use-google-analytics.tsx
+++ docs/lib/hooks/use-google-analytics.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import ReactGA from "react-ga4";
 
 export function useGoogleAnalytics() {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID;
 
   if (!GA_ID) {
     return;
