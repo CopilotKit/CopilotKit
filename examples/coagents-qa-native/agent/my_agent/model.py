8927b21d80b8c152f8658ee3f9e621327f22d3e6
Markus Ecker <markus.ecker@gmail.com>
2024-10-24 19:11:35 +0200

    add Gemini support to demos (#811)

--- examples/coagents-qa-native/agent/my_agent/model.py
+++ examples/coagents-qa-native/agent/my_agent/model.py
@@ -2,9 +2,11 @@
 This module provides a function to get a model based on the configuration.
 """
 import os
+from langchain_core.language_models.chat_models import BaseChatModel
 from my_agent.state import EmailAgentState
 
-def get_model(state: EmailAgentState):
+
+def get_model(state: EmailAgentState) -> BaseChatModel:
     """
     Get a model based on the environment variable.
     """
@@ -15,13 +17,18 @@ def get_model(state: EmailAgentState):
     print(f"Using model: {model}")
 
     if model == "openai":
-        from langchain_openai import ChatOpenAI
+        from langchain_openai import ChatOpenAI # pylint: disable=import-outside-toplevel
         return ChatOpenAI(temperature=0, model="gpt-4o-mini")
     if model == "anthropic":
-        from langchain_anthropic import ChatAnthropic
-        return ChatAnthropic(temperature=0, model="claude-3-5-sonnet-20240620")
+        from langchain_anthropic import ChatAnthropic # pylint: disable=import-outside-toplevel
+        return ChatAnthropic(
+            temperature=0, 
+            model_name="claude-3-5-sonnet-20240620",
+            timeout=None,
+            stop=None
+        )
     if model == "google_genai":
-        from langchain_google_genai import ChatGoogleGenerativeAI
+        from langchain_google_genai import ChatGoogleGenerativeAI # pylint: disable=import-outside-toplevel
         return ChatGoogleGenerativeAI(temperature=0, model="gemini-1.5-pro")
 
     raise ValueError("Invalid model specified")
--- examples/coagents-research-canvas/agent/research_canvas/model.py
+++ examples/coagents-research-canvas/agent/research_canvas/model.py
@@ -2,9 +2,11 @@
 This module provides a function to get a model based on the configuration.
 """
 import os
+from typing import cast, Any
+from langchain_core.language_models.chat_models import BaseChatModel
 from research_canvas.state import AgentState
 
-def get_model(state: AgentState):
+def get_model(state: AgentState) -> BaseChatModel:
     """
     Get a model based on the environment variable.
     """
@@ -19,9 +21,18 @@ def get_model(state: AgentState):
         return ChatOpenAI(temperature=0, model="gpt-4o-mini")
     if model == "anthropic":
         from langchain_anthropic import ChatAnthropic
-        return ChatAnthropic(temperature=0, model="claude-3-5-sonnet-20240620")
+        return ChatAnthropic(
+            temperature=0,
+            model_name="claude-3-5-sonnet-20240620",
+            timeout=None,
+            stop=None
+        )
     if model == "google_genai":
         from langchain_google_genai import ChatGoogleGenerativeAI
-        return ChatGoogleGenerativeAI(temperature=0, model="gemini-1.5-pro")
+        return ChatGoogleGenerativeAI(
+            temperature=0,
+            model="gemini-1.5-pro",
+            api_key=cast(Any, os.getenv("GOOGLE_API_KEY")) or None
+        )
 
     raise ValueError("Invalid model specified")
