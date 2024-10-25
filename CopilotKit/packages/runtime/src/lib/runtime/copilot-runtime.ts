e0ed63bf98efc32811cbbcd83b9bb0e637f3b94a
Markus Ecker <markus.ecker@gmail.com>
2024-10-25 15:40:15 +0200

    improve error handling when there is a problem with the LLM or remote actions

--- CopilotKit/packages/runtime/src/lib/runtime/copilot-runtime.ts
+++ CopilotKit/packages/runtime/src/lib/runtime/copilot-runtime.ts
@@ -167,36 +167,36 @@ export class CopilotRuntime<const T extends Parameter[] | [] = []> {
       url,
     } = request;
 
-    if (agentSession) {
-      return this.processAgentRequest(request);
-    }
-
-    const messages = rawMessages.filter((message) => !message.agentStateMessage);
-
-    const inputMessages = convertGqlInputToMessages(messages);
-    const serverSideActions = await this.getServerSideActions(request);
-
-    const serverSideActionsInput: ActionInput[] = serverSideActions.map((action) => ({
-      name: action.name,
-      description: action.description,
-      jsonSchema: JSON.stringify(actionParametersToJsonSchema(action.parameters)),
-    }));
-
-    const actionInputs = flattenToolCallsNoDuplicates([
-      ...serverSideActionsInput,
-      ...clientSideActionsInput,
-    ]);
-
-    await this.onBeforeRequest?.({
-      threadId,
-      runId,
-      inputMessages,
-      properties: graphqlContext.properties,
-      url,
-    });
+    const eventSource = new RuntimeEventSource();
 
     try {
-      const eventSource = new RuntimeEventSource();
+      if (agentSession) {
+        return await this.processAgentRequest(request);
+      }
+
+      const messages = rawMessages.filter((message) => !message.agentStateMessage);
+
+      const inputMessages = convertGqlInputToMessages(messages);
+      const serverSideActions = await this.getServerSideActions(request);
+
+      const serverSideActionsInput: ActionInput[] = serverSideActions.map((action) => ({
+        name: action.name,
+        description: action.description,
+        jsonSchema: JSON.stringify(actionParametersToJsonSchema(action.parameters)),
+      }));
+
+      const actionInputs = flattenToolCallsNoDuplicates([
+        ...serverSideActionsInput,
+        ...clientSideActionsInput,
+      ]);
+
+      await this.onBeforeRequest?.({
+        threadId,
+        runId,
+        inputMessages,
+        properties: graphqlContext.properties,
+        url,
+      });
 
       const result = await serviceAdapter.process({
         messages: inputMessages,
@@ -236,7 +236,14 @@ export class CopilotRuntime<const T extends Parameter[] | [] = []> {
       };
     } catch (error) {
       console.error("Error getting response:", error);
-      throw error;
+      eventSource.sendErrorMessageToChat();
+      return {
+        threadId: threadId || randomId(),
+        runId: runId || randomId(),
+        eventSource,
+        serverSideActions: [],
+        actionInputsWithoutAgents: [],
+      };
     }
   }
 
