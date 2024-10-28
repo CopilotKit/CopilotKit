8d286d6e74394b7b009e80ecf172fa895c6624b7
Atai Barkai <atai.barkai@gmail.com>
2024-10-27 23:40:38 -0700

    comment out CopilotKit itself

--- examples/coagents-research-canvas/ui/src/app/page.tsx
+++ examples/coagents-research-canvas/ui/src/app/page.tsx
@@ -8,13 +8,13 @@ import { ModelSelector } from "@/components/ModelSelector";
 export default function Home() {
   return (
     <ModelSelectorProvider>
-      <CopilotKit
+      {/* <CopilotKit
         runtimeUrl="/api/copilotkit"
         showDevConsole={false}
         agent="research_agent"
-      >
+      > */}
         <Main />
-      </CopilotKit>
+      {/* </CopilotKit> */}
       <ModelSelector />
     </ModelSelectorProvider>
   );
