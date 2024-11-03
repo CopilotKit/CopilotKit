ea636fccb38d02527c7e0559c73a8f4c8b8e19a3
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2024-11-03 15:40:24 -0800

    Feat/preview envs demos 1 (#886)
    * previews
    
    * wip
    
    * with preview env
    
    * fix aws region
    
    * redo
    
    * fix
    
    * remove-branch
    
    * test
    
    * update workflow
    
    * update
    
    * wip
    
    * test
    
    * add cdk deploy step
    
    * test
    
    * all stacks
    
    * use doppler
    
    * test api key
    
    * fix build
    
    * fix comment
    
    * add paths
    
    * test something
    
    * install pnpm
    
    * pnpm i
    
    * reorder
    
    * new comment
    
    * generate table
    
    * fix
    
    * test
    
    * test
    
    * add generate table script
    
    * update
    
    * get PR name
    
    * with pr number
    
    * include pr number
    
    * with log groups
    
    * update ci
    
    * add label
    
    * add additional demo to preview
    
    * disable one project
    
    * test
    
    * with doppler api keys
    
    * wip
    
    * fix
    
    * add support for coagents-qa-text
    
    * qa native demo
    
    * add more outputs
    
    * more granular

---
+++ infra/lib/utils.ts
@@ -0,0 +1,7 @@
+export function requireEnv(name: string): string {
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Required environment variable ${name} is missing`);
+  }
+  return value;
+}
