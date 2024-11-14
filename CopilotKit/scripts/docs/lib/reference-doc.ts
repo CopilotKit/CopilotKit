056afc29d8d6d6a15fe24f56ab1c6f3ef9764d38
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-11-14 13:21:40 -0600

    chore(docs): add a note at the top of reference autogenerated docfiles

--- CopilotKit/scripts/docs/lib/reference-doc.ts
+++ CopilotKit/scripts/docs/lib/reference-doc.ts
@@ -58,6 +58,14 @@ export class ReferenceDoc {
     }
     result += `---\n\n`;
 
+    result += `{\n`
+    result += ` /*\n`
+    result += `  * ATTENTION! DO NOT MODIFY THIS FILE!\n`
+    result += `  * This page is auto-generated. If you want to make any changes to this page, changes must be made at:\n`
+    result += `  * CopilotKit/${this.referenceDoc.sourcePath}\n`
+    result += `  */\n`
+    result += `}\n`
+
     result += `${comment}\n\n`;
 
     if (this.referenceDoc.hook) {
