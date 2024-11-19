9dae33d0f780cb14f256061d0e4aad33ef71cc62
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-11-18 22:56:08 -0600

    with temp file

---
+++ infra/get-e2e-tests-urls.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const json2md = require("json2md");
+
+const file = fs.readFileSync(
+  path.resolve(__dirname, "./cdk_outputs_temp.json"),
+  "utf8"
+);
+
+function generateTable() {
+  const json = JSON.parse(file);
+  const e2eUrls = {};
+
+  // Group entries by ProjectName
+  Object.values(json).forEach((value) => {
+    if (value.EndToEndProjectKey) {
+      e2eUrls[value.EndToEndProjectKey] = value.FunctionUrl;
+    }
+  }, {});
+
+  console.log(JSON.stringify(e2eUrls, null, 2));
+}
+
+generateTable();
