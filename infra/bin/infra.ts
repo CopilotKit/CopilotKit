f5f4fa04434bd7107b755969f7cf26a6b7812df6
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-11-18 21:16:24 -0600

    with all demos

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -42,179 +42,110 @@ const uiWithLocalDeps = createUIProjectStack({
   agentProject: agentWithLocalDeps
 });
 
-// const researchCanvasUIVersionedDeps = new PreviewProjectStack(app, `CoAgentsResearchCanvasDemoUIVersionedDeps`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents Research Canvas - UI",
-//   demoDir: "examples/coagents-research-canvas/ui",
-//   ecrImageTag: `coagents-research-canvas-ui-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.ui",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   environmentVariables: {
-//     REMOTE_ACTION_URL: `${researchCanvasAgent.fnUrl}/copilotkit`,
-//   },
-//   port: "3000",
-//   includeInPRComment: true,
-//   outputEnvVariable: "COAGENTS_RESEARCH_CANVAS_UI_ENDPOINT_URL"
-// });
+/*
+ * CoAgents Perplexity Demo
+ */
 
-// const researchCanvasAgent = new PreviewProjectStack(app, `CoAgentsResearchCanvasDemoAgent`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents Research Canvas - Agent",
-//   demoDir: "examples/coagents-research-canvas/agent",
-//   ecrImageTag: `coagents-research-canvas-agent-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.agent",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY", "TAVILY_API_KEY"],
-//   port: "8000",
-//   includeInPRComment: false,
-//   outputEnvVariable: "COAGENTS_RESEARCH_CANVAS_AGENT_ENDPOINT_URL",
-// });
+// Remote Dependencies
+const perplexityAgentWithRemoteDeps = createAgentProjectStack({
+  app,
+  project: "coagents-ai-researcher",
+  description: "CoAgents Perplexity Clone (Agent) - Remote Dependencies",
+  dependencies: "Remote"
+});
 
-// const researchCanvasUI = new PreviewProjectStack(app, `CoAgentsResearchCanvasDemoUI`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents Research Canvas - UI",
-//   demoDir: "examples/coagents-research-canvas/ui",
-//   ecrImageTag: `coagents-research-canvas-ui-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.ui",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   environmentVariables: {
-//     REMOTE_ACTION_URL: `${researchCanvasAgent.fnUrl}/copilotkit`,
-//   },
-//   port: "3000",
-//   includeInPRComment: true,
-//   outputEnvVariable: "COAGENTS_RESEARCH_CANVAS_UI_ENDPOINT_URL"
-// });
+const perplexityUIWithRemoteDeps = createUIProjectStack({
+  app,
+  project: "coagents-ai-researcher", 
+  description: "CoAgents Perplexity Clone (UI) - Remote Dependencies",
+  dependencies: "Remote",
+  agentProject: perplexityAgentWithRemoteDeps
+});
 
-// /**
-//  * CoAgents Perplexity Demo
-//  */
+// Local Dependencies
+const perplexityAgentWithLocalDeps = createAgentProjectStack({
+  app,
+  project: "coagents-ai-researcher",
+  description: "CoAgents Perplexity Clone (Agent) - Local Dependencies",
+  dependencies: "Local"
+});
 
-// const perplexityAgent = new PreviewProjectStack(app, `CoAgentsPerplexityDemoAgent`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents Perplexity Clone - Agent",
-//   demoDir: "examples/coagents-ai-researcher/agent",
-//   ecrImageTag: `coagents-research-canvas-agent-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.agent",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY", "TAVILY_API_KEY"],
-//   port: "8000",
-//   includeInPRComment: false,
-//   outputEnvVariable: "COAGENTS_PERPLEXITY_AGENT_ENDPOINT_URL",
-// });
+const perplexityUIWithLocalDeps = createUIProjectStack({
+  app,
+  project: "coagents-ai-researcher",
+  description: "CoAgents Perplexity Clone (UI) - Local Dependencies",
+  dependencies: "Local",
+  agentProject: perplexityAgentWithLocalDeps
+});
 
-// const perplexityUI = new PreviewProjectStack(app, `CoAgentsPerplexityDemoUI`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents Perplexity Clone - UI",
-//   demoDir: "examples/coagents-ai-researcher/ui",
-//   ecrImageTag: `coagents-ai-researcher-ui-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.ui",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   environmentVariables: {
-//     // REMOTE_ACTION_URL: `${perplexityAgent.fnUrl}/copilotkit`,
-//   },
-//   port: "3000",
-//   includeInPRComment: true,
-//   outputEnvVariable: "COAGENTS_PERPLEXITY_UI_ENDPOINT_URL",
-// });
+/*
+ * CoAgents QA Text Demo
+ */
 
-// /**
-//  * CoAgents QA Text Demo
-//  */
+// Remote Dependencies
+const qaTextAgentWithRemoteDeps = createAgentProjectStack({
+  app,
+  project: "coagents-qa-text",
+  description: "CoAgents QA Text (Agent) - Remote Dependencies",
+  dependencies: "Remote"
+});
 
-// const qaTextAgent = new PreviewProjectStack(app, `CoAgentsQATextDemoAgent`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents QA Text - Agent",
-//   demoDir: "examples/coagents-ai-researcher/agent",
-//   ecrImageTag: `coagents-ai-researcher-agent-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.agent",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY", "TAVILY_API_KEY"],
-//   port: "8000",
-//   includeInPRComment: false,
-//   outputEnvVariable: "COAGENTS_QA_TEXT_AGENT_ENDPOINT_URL",
-// });
+const qaTextUIWithRemoteDeps = createUIProjectStack({
+  app,
+  project: "coagents-qa-text",
+  description: "CoAgents QA Text (UI) - Remote Dependencies",
+  dependencies: "Remote",
+  agentProject: qaTextAgentWithRemoteDeps
+});
 
-// const qaTextUI = new PreviewProjectStack(app, `CoAgentsQATextDemoUI`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents QA Text - UI",
-//   demoDir: "examples/coagents-qa-text/ui",
-//   ecrImageTag: `coagents-qa-text-ui-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.ui",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   environmentVariables: {
-//     // REMOTE_ACTION_URL: `${qaTextAgent.fnUrl}/copilotkit`,
-//   },
-//   port: "3000",
-//   includeInPRComment: true,
-//   outputEnvVariable: "COAGENTS_QA_TEXT_UI_ENDPOINT_URL",
-// });
+// Local Dependencies
+const qaTextAgentWithLocalDeps = createAgentProjectStack({
+  app,
+  project: "coagents-qa-text",
+  description: "CoAgents QA Text (Agent) - Local Dependencies",
+  dependencies: "Local"
+});
 
-// /**
-//  * CoAgents QA Native Demo
-//  */
+const qaTextUIWithLocalDeps = createUIProjectStack({
+  app,
+  project: "coagents-qa-text",
+  description: "CoAgents QA Text (UI) - Local Dependencies",
+  dependencies: "Local",
+  agentProject: qaTextAgentWithLocalDeps
+});
 
-// const qaNativeAgent = new PreviewProjectStack(app, `CoAgentsQANativetDemoAgent`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents QA Native - Agent",
-//   demoDir: "examples/coagents-ai-researcher/agent",
-//   ecrImageTag: `coagents-qa-native-agent-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.agent",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY", "TAVILY_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   port: "8000",
-//   includeInPRComment: false,
-//   outputEnvVariable: "COAGENTS_QA_NATIVE_AGENT_ENDPOINT_URL",
-// });
+/*
+ * CoAgents QA Native Demo
+ */
 
-// const qaNativeAUI = new PreviewProjectStack(app, `CoAgentsQANativeDemoUI`, {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//   },
-//   projectName: "CoAgents QA Native - UI",
-//   demoDir: "examples/coagents-qa-native/ui",
-//   ecrImageTag: `coagents-qa-native-ui-${githubRunId}`,
-//   overrideDockerWorkdir: "./",
-//   overrideDockerfile: "examples/Dockerfile.ui",
-//   uniqueEnvironmentId,
-//   environmentVariablesFromSecrets: ["OPENAI_API_KEY"],
-//   buildSecrets: ["OPENAI_API_KEY"],
-//   environmentVariables: {
-//     // REMOTE_ACTION_URL: `${qaNativeAgent.fnUrl}/copilotkit`,
-//   },
-//   port: "3000",
-//   includeInPRComment: true,
-//   outputEnvVariable: "COAGENTS_QA_NATIVE_UI_ENDPOINT_URL",
-// });
+// Remote Dependencies
+const qaNativeAgentWithRemoteDeps = createAgentProjectStack({
+  app,
+  project: "coagents-qa-native",
+  description: "CoAgents QA Native (Agent) - Remote Dependencies",
+  dependencies: "Remote"
+});
+
+const qaNativeUIWithRemoteDeps = createUIProjectStack({
+  app,
+  project: "coagents-qa-native",
+  description: "CoAgents QA Native (UI) - Remote Dependencies",
+  dependencies: "Remote",
+  agentProject: qaNativeAgentWithRemoteDeps
+});
+
+// Local Dependencies
+const qaNativeAgentWithLocalDeps = createAgentProjectStack({
+  app,
+  project: "coagents-qa-native",
+  description: "CoAgents QA Native (Agent) - Local Dependencies",
+  dependencies: "Local"
+});
+
+const qaNativeUIWithLocalDeps = createUIProjectStack({
+  app,
+  project: "coagents-qa-native",
+  description: "CoAgents QA Native (UI) - Local Dependencies",
+  dependencies: "Local",
+  agentProject: qaNativeAgentWithLocalDeps
+});
