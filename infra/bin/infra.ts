9d5d6c4824d8a0aa7e6ed4f2546a82f4261f35f3
Ariel Weinberger <weinberger.ariel@gmail.com>
2024-11-27 15:32:46 -0600

    disable other preview deps

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -10,85 +10,85 @@ const app = new cdk.App();
  * Research Canvas
  */
 
-const coAgentsResearchCanvasAgentWithLocalDeps = createAgentProjectStack({
-  app,
-  project: "coagents-research-canvas",
-  description: "CoAgents Research Canvas (Agent) - Local Depenencies",
-  dependencies: "Local"
-});
+// const coAgentsResearchCanvasAgentWithLocalDeps = createAgentProjectStack({
+//   app,
+//   project: "coagents-research-canvas",
+//   description: "CoAgents Research Canvas (Agent) - Local Depenencies",
+//   dependencies: "Local"
+// });
 
-const coAgentsResearchCanvasUIWithLocalDeps = createUIProjectStack({
-  app,
-  project: "coagents-research-canvas",
-  description: "CoAgents Research Canvas (UI) - Local Depenencies",
-  dependencies: "Local",
-  selfHostedAgentProject: coAgentsResearchCanvasAgentWithLocalDeps.selfHostedAgent,
-  lgcAgentProjectPython: coAgentsResearchCanvasAgentWithLocalDeps.lgcAgentPython,
-  environmentVariables: {}
-});
+// const coAgentsResearchCanvasUIWithLocalDeps = createUIProjectStack({
+//   app,
+//   project: "coagents-research-canvas",
+//   description: "CoAgents Research Canvas (UI) - Local Depenencies",
+//   dependencies: "Local",
+//   selfHostedAgentProject: coAgentsResearchCanvasAgentWithLocalDeps.selfHostedAgent,
+//   lgcAgentProjectPython: coAgentsResearchCanvasAgentWithLocalDeps.lgcAgentPython,
+//   environmentVariables: {}
+// });
 
-/*
- * CoAgents Routing Demo
- */
+// /*
+//  * CoAgents Routing Demo
+//  */
 
-const coAgentsRoutingAgentWithLocalDeps = createAgentProjectStack({
-  app,
-  project: "coagents-routing",
-  description: "CoAgents Routing (Agent) - Local Dependencies",
-  dependencies: "Local"
-});
+// const coAgentsRoutingAgentWithLocalDeps = createAgentProjectStack({
+//   app,
+//   project: "coagents-routing",
+//   description: "CoAgents Routing (Agent) - Local Dependencies",
+//   dependencies: "Local"
+// });
 
-const coAgentsRoutingUIWithLocalDeps = createUIProjectStack({
-  app,
-  project: "coagents-routing",
-  description: "CoAgents Routing (UI) - Local Dependencies",
-  dependencies: "Local",
-  selfHostedAgentProject: coAgentsRoutingAgentWithLocalDeps.selfHostedAgent,
-  lgcAgentProjectPython: coAgentsRoutingAgentWithLocalDeps.lgcAgentPython,
-  environmentVariables: {}
-});
+// const coAgentsRoutingUIWithLocalDeps = createUIProjectStack({
+//   app,
+//   project: "coagents-routing",
+//   description: "CoAgents Routing (UI) - Local Dependencies",
+//   dependencies: "Local",
+//   selfHostedAgentProject: coAgentsRoutingAgentWithLocalDeps.selfHostedAgent,
+//   lgcAgentProjectPython: coAgentsRoutingAgentWithLocalDeps.lgcAgentPython,
+//   environmentVariables: {}
+// });
 
-/*
- * CoAgents QA Text Demo
- */
+// /*
+//  * CoAgents QA Text Demo
+//  */
 
-const qaTextAgentWithLocalDeps = createAgentProjectStack({
-  app,
-  project: "coagents-qa-text",
-  description: "CoAgents QA Text (Agent) - Local Dependencies",
-  dependencies: "Local"
-});
+// const qaTextAgentWithLocalDeps = createAgentProjectStack({
+//   app,
+//   project: "coagents-qa-text",
+//   description: "CoAgents QA Text (Agent) - Local Dependencies",
+//   dependencies: "Local"
+// });
 
-const qaTextUIWithLocalDeps = createUIProjectStack({
-  app,
-  project: "coagents-qa-text",
-  description: "CoAgents QA Text (UI) - Local Dependencies",
-  dependencies: "Local",
-  selfHostedAgentProject: qaTextAgentWithLocalDeps.selfHostedAgent,
-  lgcAgentProjectPython: qaTextAgentWithLocalDeps.lgcAgentPython,
-  environmentVariables: {}
-});
+// const qaTextUIWithLocalDeps = createUIProjectStack({
+//   app,
+//   project: "coagents-qa-text",
+//   description: "CoAgents QA Text (UI) - Local Dependencies",
+//   dependencies: "Local",
+//   selfHostedAgentProject: qaTextAgentWithLocalDeps.selfHostedAgent,
+//   lgcAgentProjectPython: qaTextAgentWithLocalDeps.lgcAgentPython,
+//   environmentVariables: {}
+// });
 
-/*
- * CoAgents QA Native Demo
- */
+// /*
+//  * CoAgents QA Native Demo
+//  */
 
-const qaNativeAgentWithLocalDeps = createAgentProjectStack({
-  app,
-  project: "coagents-qa-native",
-  description: "CoAgents QA Native (Agent) - Local Dependencies",
-  dependencies: "Local"
-});
+// const qaNativeAgentWithLocalDeps = createAgentProjectStack({
+//   app,
+//   project: "coagents-qa-native",
+//   description: "CoAgents QA Native (Agent) - Local Dependencies",
+//   dependencies: "Local"
+// });
 
-const qaNativeUIWithLocalDeps = createUIProjectStack({
-  app,
-  project: "coagents-qa-native",
-  description: "CoAgents QA Native (UI) - Local Dependencies",
-  dependencies: "Local",
-  selfHostedAgentProject: qaNativeAgentWithLocalDeps.selfHostedAgent,
-  lgcAgentProjectPython: qaNativeAgentWithLocalDeps.lgcAgentPython,
-  environmentVariables: {}
-});
+// const qaNativeUIWithLocalDeps = createUIProjectStack({
+//   app,
+//   project: "coagents-qa-native",
+//   description: "CoAgents QA Native (UI) - Local Dependencies",
+//   dependencies: "Local",
+//   selfHostedAgentProject: qaNativeAgentWithLocalDeps.selfHostedAgent,
+//   lgcAgentProjectPython: qaNativeAgentWithLocalDeps.lgcAgentPython,
+//   environmentVariables: {}
+// });
 
 /**
  * Next OpenAI Demo
