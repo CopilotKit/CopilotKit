9d00076df414f85a4acd345bcb991f55fc78a99a
Markus Ecker <markus.ecker@gmail.com>
2024-11-29 17:41:28 +0100

    support streaming multiple messages and refactor rxjs API (#978)
    * support streaming multiple messages and refactor rxjs API

--- CopilotKit/packages/runtime/src/service-adapters/langchain/utils.ts
+++ CopilotKit/packages/runtime/src/service-adapters/langchain/utils.ts
@@ -129,11 +129,11 @@ function maybeSendActionExecutionResultIsMessage(
   // language models need a result after the function call
   // we simply let them know that we are sending a message
   if (actionExecution) {
-    eventStream$.sendActionExecutionResult(
-      actionExecution.id,
-      actionExecution.name,
-      "Sending a message",
-    );
+    eventStream$.sendActionExecutionResult({
+      actionExecutionId: actionExecution.id,
+      actionName: actionExecution.name,
+      result: "Sending a message",
+    });
   }
 }
 
@@ -152,7 +152,11 @@ export async function streamLangChainResponse({
       eventStream$.sendTextMessage(randomId(), result);
     } else {
       // Send as a result
-      eventStream$.sendActionExecutionResult(actionExecution.id, actionExecution.name, result);
+      eventStream$.sendActionExecutionResult({
+        actionExecutionId: actionExecution.id,
+        actionName: actionExecution.name,
+        result: result,
+      });
     }
   }
 
@@ -165,11 +169,11 @@ export async function streamLangChainResponse({
       eventStream$.sendTextMessage(randomId(), result.content as string);
     }
     for (const toolCall of result.tool_calls) {
-      eventStream$.sendActionExecution(
-        toolCall.id || randomId(),
-        toolCall.name,
-        JSON.stringify(toolCall.args),
-      );
+      eventStream$.sendActionExecution({
+        actionExecutionId: toolCall.id || randomId(),
+        actionName: toolCall.name,
+        args: JSON.stringify(toolCall.args),
+      });
     }
   }
 
@@ -183,11 +187,11 @@ export async function streamLangChainResponse({
     }
     if (result.lc_kwargs?.tool_calls) {
       for (const toolCall of result.lc_kwargs?.tool_calls) {
-        eventStream$.sendActionExecution(
-          toolCall.id || randomId(),
-          toolCall.name,
-          JSON.stringify(toolCall.args),
-        );
+        eventStream$.sendActionExecution({
+          actionExecutionId: toolCall.id || randomId(),
+          actionName: toolCall.name,
+          args: JSON.stringify(toolCall.args),
+        });
       }
     }
   }
@@ -214,6 +218,7 @@ export async function streamLangChainResponse({
 
         let toolCallName: string | undefined = undefined;
         let toolCallId: string | undefined = undefined;
+        let currentMessageId: string;
         let toolCallArgs: string | undefined = undefined;
         let hasToolCall: boolean = false;
         let content = value?.content as string;
@@ -248,10 +253,10 @@ export async function streamLangChainResponse({
         // If toolCallName is defined, it means a new tool call starts.
         if (mode === "message" && (toolCallId || done)) {
           mode = null;
-          eventStream$.sendTextMessageEnd();
+          eventStream$.sendTextMessageEnd({ messageId: currentMessageId });
         } else if (mode === "function" && (!hasToolCall || done)) {
           mode = null;
-          eventStream$.sendActionExecutionEnd();
+          eventStream$.sendActionExecutionEnd({ actionExecutionId: toolCallId });
         }
 
         if (done) {
@@ -262,26 +267,37 @@ export async function streamLangChainResponse({
         if (mode === null) {
           if (hasToolCall && toolCallId && toolCallName) {
             mode = "function";
-            eventStream$.sendActionExecutionStart(toolCallId, toolCallName);
+            eventStream$.sendActionExecutionStart({
+              actionExecutionId: toolCallId,
+              actionName: toolCallName,
+            });
           } else if (content) {
             mode = "message";
-            eventStream$.sendTextMessageStart(randomId());
+            currentMessageId = randomId();
+            eventStream$.sendTextMessageStart({ messageId: currentMessageId });
           }
         }
 
         // send the content events
         if (mode === "message" && content) {
-          eventStream$.sendTextMessageContent(
-            Array.isArray(content) ? (content[0]?.text ?? "") : content,
-          );
+          eventStream$.sendTextMessageContent({
+            messageId: currentMessageId,
+            content: Array.isArray(content) ? (content[0]?.text ?? "") : content,
+          });
         } else if (mode === "function" && toolCallArgs) {
           // For calls of the same tool with different index, we seal last tool call and register a new one
           if (toolCallDetails.index !== toolCallDetails.prevIndex) {
-            eventStream$.sendActionExecutionEnd();
-            eventStream$.sendActionExecutionStart(toolCallId, toolCallName);
+            eventStream$.sendActionExecutionEnd({ actionExecutionId: toolCallId });
+            eventStream$.sendActionExecutionStart({
+              actionExecutionId: toolCallId,
+              actionName: toolCallName,
+            });
             toolCallDetails.prevIndex = toolCallDetails.index;
           }
-          eventStream$.sendActionExecutionArgs(toolCallArgs);
+          eventStream$.sendActionExecutionArgs({
+            actionExecutionId: toolCallId,
+            args: toolCallArgs,
+          });
         }
       } catch (error) {
         console.error("Error reading from stream", error);
@@ -289,11 +305,11 @@ export async function streamLangChainResponse({
       }
     }
   } else if (actionExecution) {
-    eventStream$.sendActionExecutionResult(
-      actionExecution.id,
-      actionExecution.name,
-      encodeResult(result),
-    );
+    eventStream$.sendActionExecutionResult({
+      actionExecutionId: actionExecution.id,
+      actionName: actionExecution.name,
+      result: encodeResult(result),
+    });
   }
 
   // unsupported type
