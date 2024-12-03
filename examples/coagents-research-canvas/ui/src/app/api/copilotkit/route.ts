464445bb94669624d136274155d91218292eeb56
Suhas Deshpande <suhas2u@gmail.com>
2024-12-03 09:40:13 -0800

    fixup import in coagent-research-canvas

--- examples/coagents-research-canvas/ui/src/app/api/copilotkit/route.ts
+++ examples/coagents-research-canvas/ui/src/app/api/copilotkit/route.ts
@@ -2,36 +2,40 @@ import {
   CopilotRuntime,
   OpenAIAdapter,
   copilotRuntimeNextJSAppRouterEndpoint,
-  langGraphPlatformEndpoint, copilotKitEndpoint,
+  langGraphCloudEndpoint,
+  copilotKitEndpoint,
 } from "@copilotkit/runtime";
 import OpenAI from "openai";
 import { NextRequest } from "next/server";
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const llmAdapter = new OpenAIAdapter({ openai } as any);
-const langsmithApiKey = process.env.LANGSMITH_API_KEY as string
+const langsmithApiKey = process.env.LANGSMITH_API_KEY as string;
 
 export const POST = async (req: NextRequest) => {
-  const searchParams = req.nextUrl.searchParams
-  const deploymentUrl = searchParams.get('lgcDeploymentUrl')
+  const searchParams = req.nextUrl.searchParams;
+  const deploymentUrl = searchParams.get("lgcDeploymentUrl");
 
-  const remoteEndpoint = deploymentUrl ? langGraphPlatformEndpoint({
-    deploymentUrl,
-    langsmithApiKey,
-    agents: [
-      {
-        name: "research_agent",
-        description: "Research agent",
-      },
-      {
-        name: "research_agent_google_genai",
-        description: "Research agent",
-        assistantId: "9dc0ca3b-1aa6-547d-93f0-e21597d2011c",
-      },
-    ],
-  }) : copilotKitEndpoint({
-    url: process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",
-  })
+  const remoteEndpoint = deploymentUrl
+    ? langGraphCloudEndpoint({
+        deploymentUrl,
+        langsmithApiKey,
+        agents: [
+          {
+            name: "research_agent",
+            description: "Research agent",
+          },
+          {
+            name: "research_agent_google_genai",
+            description: "Research agent",
+            assistantId: "9dc0ca3b-1aa6-547d-93f0-e21597d2011c",
+          },
+        ],
+      })
+    : copilotKitEndpoint({
+        url:
+          process.env.REMOTE_ACTION_URL || "http://localhost:8000/copilotkit",
+      });
 
   const runtime = new CopilotRuntime({
     remoteEndpoints: [remoteEndpoint],
