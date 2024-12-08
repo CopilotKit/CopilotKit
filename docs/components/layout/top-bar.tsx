7ef999f327ee9e0277d1c1b5d9a3cb97d3d48b90
Tyler Slaton <tyler@copilotkit.ai>
2024-12-08 18:21:26 -0500

    docs: adding a lot of polish to the docs
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- docs/components/layout/top-bar.tsx
+++ docs/components/layout/top-bar.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { SearchIcon } from "lucide-react";
-// import { LinkToCopilotCloud } from "../react/link-to-copilot-cloud";
+import { FaDiscord } from "react-icons/fa";
+import { Socials } from "@/components/react/socials";
+import { LinkToCopilotCloud } from "../react/link-to-copilot-cloud";
 
 export function TopBar() {
   return (
     <>
       <div className="p-2 h-[60px] hidden lg:block absolute w-[calc(100vw-var(--fd-sidebar-width)-20px)] ml-[var(--fd-sidebar-width)]">
-        <div className="flex justify-end items-center gap-4">
-          {/* <LinkToCopilotCloud />
-          <div className="h-8 border-l border-secondary-foreground/20 border-1.5" /> */}
-          <SearchToggle />
+        <div className="flex justify-between items-center gap-2">
+          <Socials />
+          <div className="flex justify-end items-center gap-2">
+            {/* <LinkToCopilotCloud />
+            <div className="h-8 border-l border-secondary-foreground/20 border-1.5" /> */}
+            <SearchToggle />
+          </div>
         </div>
       </div>
     </>
@@ -18,7 +23,7 @@ export function TopBar() {
 
 export function SearchToggle() {
   return (
-    <div onClick={toggleSearch} className="cursor-pointer h-10 w-[400px] inline-flex items-center gap-2 border bg-fd-secondary/50 p-1.5 text-sm text-fd-muted-foreground transition-colors hover:bg-fd-accent hover:text-fd-accent-foreground rounded-lg max-md:hidden">
+    <div onClick={toggleSearch} className="cursor-pointer h-10 w-[240px] inline-flex items-center gap-2 border bg-fd-secondary/50 p-1.5 text-sm text-fd-muted-foreground transition-colors hover:bg-fd-accent hover:text-fd-accent-foreground rounded-lg max-md:hidden">
       <SearchIcon className="w-4 h-4" />
       Search
       <div className="ms-auto inline-flex gap-0.5">
