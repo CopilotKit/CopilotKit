efb3bc488b6a6f1eaf743bb1009c911b273ce7af
Ran Shem Tov <ran@copilotkit.ai>
2024-12-10 09:52:28 +0100

    feat(dev-console): disable form

--- CopilotKit/packages/react-ui/src/components/help-modal/modal.tsx
+++ CopilotKit/packages/react-ui/src/components/help-modal/modal.tsx
@@ -29,17 +29,19 @@ export function CopilotKitHelpModal() {
                 >
                     <div className="bg-white rounded-lg shadow-xl max-w-md w-full p-4 flex-col relative">
                         <button
-                            className="absolute top-2.5 right-2.5 text-gray-400 hover:text-gray-600 focus:outline-none"
+                            className="absolute text-gray-400 hover:text-gray-600 focus:outline-none"
+                            style={{top: "10px", right: "10px"}}
                             onClick={() => setShowHelpModal(false)}
                             aria-label="Close"
                         >
-                            <CloseIcon />
+                            <CloseIcon/>
                         </button>
                         <div className="w-full flex mb-6 justify-center">
                             <h2 className="text-2xl font-bold">Help Options</h2>
                         </div>
                         <div className="space-y-4 mb-4">
-                            <div className="block w-full text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-150 text-sm">
+                            <div
+                                className="block w-full text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-150 text-sm">
                                 <a
                                     href="https://go.copilotkit.ai/dev-console-support-discord"
                                     target="_blank"
@@ -48,7 +50,8 @@ export function CopilotKitHelpModal() {
                                     Go to Discord Support Channel (Community Support)
                                 </a>
                             </div>
-                            <div className="block w-full text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-150 text-sm">
+                            <div
+                                className="block w-full text-center py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-150 text-sm">
                                 <a
                                     href="https://go.copilotkit.ai/dev-console-support-slack"
                                     target="_blank"
@@ -57,29 +60,30 @@ export function CopilotKitHelpModal() {
                                     Apply for Priority Direct Slack Support
                                 </a>
                             </div>
-                            <div>
-                                <label htmlFor="feedback" className="block text-sm font-medium text-gray-700 mb-1">
-                                    Let us know what your issue is:
-                                </label>
-                                <textarea
-                                    id="feedback"
-                                    rows={4}
-                                    className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500"
-                                    placeholder="A Loom link / screen recording is always great!"
-                                    onChange={e => setIssueDescription(e.target.value)}
-                                    value={issueDescription}
-                                ></textarea>
-                            </div>
                         </div>
-                        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-b-lg">
-                            <button
-                                type="button"
-                                onClick={handleSubmit}
-                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                            >
-                                Submit
-                            </button>
-                        </div>
+                        {/*<form onSubmit={handleSubmit} className="flex flex-col space-y-4">*/}
+                        {/*    <div>*/}
+                        {/*        <label htmlFor="feedback" className="block text-sm font-medium text-gray-700 mb-1">*/}
+                        {/*            Let us know what your issue is:*/}
+                        {/*        </label>*/}
+                        {/*        <textarea*/}
+                        {/*            id="feedback"*/}
+                        {/*            rows={4}*/}
+                        {/*            className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500"*/}
+                        {/*            placeholder="A Loom link / screen recording is always great!"*/}
+                        {/*            onChange={e => setIssueDescription(e.target.value)}*/}
+                        {/*            value={issueDescription}*/}
+                        {/*        ></textarea>*/}
+                        {/*    </div>*/}
+                        {/*    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-b-lg">*/}
+                        {/*        <button*/}
+                        {/*            type="submit"*/}
+                        {/*            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"*/}
+                        {/*        >*/}
+                        {/*            Submit*/}
+                        {/*        </button>*/}
+                        {/*    </div>*/}
+                        {/*</form>*/}
                     </div>
                 </div>
             )}
