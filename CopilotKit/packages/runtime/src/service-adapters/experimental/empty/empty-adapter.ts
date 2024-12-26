4ca48b6ba36b902d0d586b31976f39679eca6f56
Toth-Csaba <toth_csaba@bah.com>
2024-12-26 09:51:56 -0800

    Empty adapter use exmaple in code docs by @ranst91

--- CopilotKit/packages/runtime/src/service-adapters/experimental/empty/empty-adapter.ts
+++ CopilotKit/packages/runtime/src/service-adapters/experimental/empty/empty-adapter.ts
@@ -4,6 +4,16 @@
  * This adapter is meant to preserve adherence to runtime requirements, while doing nothing
  * Ideal if you don't want to connect an LLM the to the runtime, and only use your LangGraph agent.
  * Be aware that Copilot Suggestions will not work if you use this adapter
+ *
+ * ## Example
+ *
+ * ```ts
+ * import { CopilotRuntime, ExperimentalEmptyAdapter } from "@copilotkit/runtime";
+ *
+ * const copilotKit = new CopilotRuntime();
+ *
+ * return new ExperimentalEmptyAdapter();
+ * ```
  */
 import {
   CopilotServiceAdapter,
