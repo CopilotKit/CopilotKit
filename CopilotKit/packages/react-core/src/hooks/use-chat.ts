c61b1c658e0d6e5fce6196a2519d0e80a9b1f32d
Suhas Deshpande <suhas2u@gmail.com>
2025-01-08 10:47:19 -0800

    fix prettier

--- CopilotKit/packages/react-core/src/hooks/use-chat.ts
+++ CopilotKit/packages/react-core/src/hooks/use-chat.ts
@@ -105,7 +105,7 @@ export type UseChatOptions = {
    * The forwarded parameters.
    */
   forwardedParameters?: Pick<ForwardedParametersInput, "temperature">;
-  
+
   /**
    * The current thread ID.
    */
