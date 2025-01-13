86fae3aa25a09eadf4cb88005def2a1505597087
Tyler Slaton <54378333+tylerslaton@users.noreply.github.com>
2025-01-13 11:14:10 -0500

    feat(react-ui): add interfaces for full message customization (#1216)
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

---
+++ CopilotKit/.changeset/tender-horses-begin.md
@@ -0,0 +1,7 @@
+---
+"@copilotkit/react-ui": patch
+---
+
+- feat(react-ui): add interfaces for full message customization
+
+Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>
--- CopilotKit/packages/react-ui/src/components/chat/index.tsx
+++ CopilotKit/packages/react-ui/src/components/chat/index.tsx
@@ -2,4 +2,7 @@ export * from "./props";
 export { CopilotPopup } from "./Popup";
 export { CopilotSidebar } from "./Sidebar";
 export { CopilotChat } from "./Chat";
+export { Markdown } from "./Markdown";
+export { AssistantMessage } from "./messages/AssistantMessage";
+export { UserMessage } from "./messages/UserMessage";
 export { useChatContext } from "./ChatContext";
