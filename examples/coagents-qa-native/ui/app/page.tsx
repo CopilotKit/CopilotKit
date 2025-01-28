52fd4f35080797a5a7b2d3ca65a1907062bdc715
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-01-28 15:24:38 -0600

    add support for dynamic runtime and public api key for all other demos

--- examples/coagents-qa-native/ui/app/page.tsx
+++ examples/coagents-qa-native/ui/app/page.tsx
@@ -5,6 +5,7 @@ import { Mailer } from "./Mailer";
 import "@copilotkit/react-ui/styles.css";
 import { ModelSelectorProvider, useModelSelectorContext } from "@/lib/model-selector-provider";
 import { ModelSelector } from "@/components/ModelSelector";
+import { useSearchParams } from "next/navigation";
 
 export default function ModelSelectorWrapper() {
     return (
@@ -20,8 +21,22 @@ export default function ModelSelectorWrapper() {
 function Home() {
   const { lgcDeploymentUrl } = useModelSelectorContext();
 
+  const searchParams = useSearchParams();
+
+  const runtimeUrl = searchParams.get("runtimeUrl")
+    ? (searchParams.get("runtimeUrl") as string)
+    : `/api/copilotkit?lgcDeploymentUrl=${lgcDeploymentUrl ?? ""}`;
+
+  const publicApiKey = searchParams.get("publicApiKey");
+  const copilotKitProps: Partial<React.ComponentProps<typeof CopilotKit>> = {
+    runtimeUrl,
+    publicApiKey: publicApiKey || undefined,
+    showDevConsole: true,
+    agent: "email_agent",
+  };
+  
   return (
-      <CopilotKit runtimeUrl={`/api/copilotkit?lgcDeploymentUrl=${lgcDeploymentUrl ?? ''}`} agent="email_agent">
+      <CopilotKit {...copilotKitProps}>
           <Mailer />
       </CopilotKit>
   );
