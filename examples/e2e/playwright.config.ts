dc19fe23a6898e55ed6bd1c9add3025e4c41b85c
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-01-28 16:12:22 -0600

    adjust configuration for retries

--- examples/e2e/playwright.config.ts
+++ examples/e2e/playwright.config.ts
@@ -12,16 +12,16 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 100000,
+  timeout: 10000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 6 : 3,
+  retries: process.env.CI ? 10 : 3,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
