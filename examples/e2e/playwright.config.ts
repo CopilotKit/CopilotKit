f77939e05bba1d3d88e4baf68c175e542c96a857
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-01-28 16:30:30 -0600

    tweak playwright config

--- examples/e2e/playwright.config.ts
+++ examples/e2e/playwright.config.ts
@@ -12,14 +12,14 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 10000,
+  timeout: 60000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 10 : 3,
+  retries: process.env.CI ? 8 : 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
