be774bd061969d763ddc8ea0dc0956b304972a81
Ran Shem Tov <ran@copilotkit.ai>
2025-02-06 14:20:58 +0100

    chore: attempt fix gql generation BEFORE build

--- CopilotKit/packages/runtime-client-gql/tsup.config.ts
+++ CopilotKit/packages/runtime-client-gql/tsup.config.ts
@@ -1,20 +1,4 @@
 import { defineConfig, Options } from "tsup";
-import { Plugin } from "esbuild";
-import { generate } from "@graphql-codegen/cli";
-import codegenConfig from "./codegen";
-
-const runBeforeBuildPlugin: Plugin = {
-  name: "run-before-build",
-  setup(build) {
-    const prefix = build.initialOptions.format;
-
-    build.onStart(async () => {
-      console.log(`[${prefix}] Running graphql-codegen`);
-      await generate(codegenConfig);
-      console.log(`[${prefix}] graphql-codegen completed successfully`);
-    });
-  },
-};
 
 export default defineConfig((options: Options) => ({
   entry: ["src/**/*.{ts,tsx}"],
@@ -24,10 +8,9 @@ export default defineConfig((options: Options) => ({
   external: ["react"],
   sourcemap: true,
   exclude: [
-    "**/*.test.ts", // Exclude TypeScript test files
-    "**/*.test.tsx", // Exclude TypeScript React test files
-    "**/__tests__/*", // Exclude any files inside a __tests__ directory
+    "**/*.test.ts",
+    "**/*.test.tsx",
+    "**/__tests__/*",
   ],
-  esbuildPlugins: [runBeforeBuildPlugin as any],
   ...options,
 }));
