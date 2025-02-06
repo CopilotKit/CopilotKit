c9ae3050a23667b22b172f784846e4209bb9a81f
Suhas Deshpande <suhasdeshpande@users.noreply.github.com>
2025-02-05 21:13:40 -0800

    perf: prevent redundant API calls (#1290)

--- CopilotKit/packages/react-core/src/components/copilot-provider/copilot-messages.tsx
+++ CopilotKit/packages/react-core/src/components/copilot-provider/copilot-messages.tsx
@@ -2,35 +2,50 @@
  * An internal context to separate the messages state (which is constantly changing) from the rest of CopilotKit context
  */
 
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useRef } from "react";
 import { CopilotMessagesContext } from "../../context/copilot-messages-context";
 import { loadMessagesFromJsonRepresentation, Message } from "@copilotkit/runtime-client-gql";
-import { CopilotKitProps } from "./copilotkit-props";
 import { useCopilotContext } from "../../context/copilot-context";
 
 export function CopilotMessages({ children }: { children: ReactNode }) {
   const [messages, setMessages] = useState<Message[]>([]);
+  const lastLoadedThreadId = useRef<string>();
+  const lastLoadedAgentName = useRef<string>();
+  const lastLoadedMessages = useRef<string>();
 
   const { threadId, agentSession, runtimeClient } = useCopilotContext();
 
   useEffect(() => {
-    if (agentSession?.agentName) {
-      // reload messages
-      const fetchAgentState = async () => {
-        const result = await runtimeClient.loadAgentState({
-          threadId,
-          agentName: agentSession.agentName,
-        });
-        if (result.data?.loadAgentState?.threadExists) {
-          const messages = loadMessagesFromJsonRepresentation(
-            JSON.parse(result.data?.loadAgentState?.messages || "[]"),
-          );
-          setMessages(messages);
-        }
-      };
-      void fetchAgentState();
+    if (!threadId || threadId === lastLoadedThreadId.current) return;
+    if (
+      threadId === lastLoadedThreadId.current &&
+      agentSession?.agentName === lastLoadedAgentName.current
+    ) {
+      return;
     }
-  }, [threadId, agentSession?.agentName !== undefined]);
+
+    const fetchMessages = async () => {
+      if (!agentSession?.agentName) return;
+
+      const result = await runtimeClient.loadAgentState({
+        threadId,
+        agentName: agentSession?.agentName,
+      });
+
+      const newMessages = result.data?.loadAgentState?.messages;
+      if (newMessages === lastLoadedMessages.current) return;
+
+      if (result.data?.loadAgentState?.threadExists) {
+        lastLoadedMessages.current = newMessages;
+        lastLoadedThreadId.current = threadId;
+        lastLoadedAgentName.current = agentSession?.agentName;
+
+        const messages = loadMessagesFromJsonRepresentation(JSON.parse(newMessages || "[]"));
+        setMessages(messages);
+      }
+    };
+    void fetchMessages();
+  }, [threadId, agentSession?.agentName]);
 
   return (
     <CopilotMessagesContext.Provider
