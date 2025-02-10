16a6e1439e55abc72265541b1abbe48df137e9eb
Tyler Slaton <54378333+tylerslaton@users.noreply.github.com>
2025-02-10 18:54:53 -0500

    refactor(docs): reorganize, repurpose and generally improve documentation experience
    - All CoAgents documentation has been improved or reevaluated in some way
    - The coaents-starter has been simplified
    - The coagents-starter now has a JS agent
    - Expanded and unified examples in guides
    - Quickstart has been improved in a number of ways
    - Adding documentation about LangGraph interrupt
    
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -29,7 +29,102 @@ const config = {
         source: '/coagents/react-ui/frontend-functions',
         destination: '/coagents/react-ui/hitl',
         permanent: true,
-      }
+      },
+      {
+        source: '/coagents/quickstart',
+        destination: '/coagents/quickstart/langgraph',
+        permanent: true,
+      },
+      {
+        source: '/coagents/chat-ui/render-agent-state',
+        destination: '/coagents/generative-ui/agentic',
+        permanent: true,
+      },
+      {
+        source: '/coagents/chat-ui/hitl',
+        destination: '/coagents/human-in-the-loop/node-flow',
+        permanent: true,
+      },
+      {
+        source: '/coagents/chat-ui/hitl/interrupt-flow',
+        destination: '/coagents/human-in-the-loop/interrupt-flow',
+        permanent: true,
+      },
+      {
+        source: '/coagents/chat-ui/loading-message-history',
+        destination: '/coagents/persistence/loading-message-history',
+        permanent: true,
+      },
+      {
+        source: '/coagents/react-ui/in-app-agent-read',
+        destination: '/coagents/shared-state/in-app-agent-read',
+        permanent: true,
+      },
+      {
+        source: '/coagents/react-ui/in-app-agent-write',
+        destination: '/coagents/shared-state/in-app-agent-write',
+        permanent: true,
+      },
+      {
+        source: '/coagents/react-ui/hitl',
+        destination: '/coagents/human-in-the-loop/node-flow',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/router-mode-agent-lock',
+        destination: '/coagents/multi-agent-flows',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/intermediate-state-streaming',
+        destination: '/coagents/shared-state/predictive-state-updates',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/manually-emitting-messages',
+        destination: '/coagents/advanced/emit-messages',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/state-streaming',
+        destination: '/coagents/shared-state',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/copilotkit-state',
+        destination: '/coagents/frontend-actions',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/message-persistence',
+        destination: '/coagents/persistence/message-persistence',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/loading-message-history',
+        destination: '/coagents/persistence/loading-message-history',
+        permanent: true,
+      },
+      {
+        source: '/coagents/advanced/loading-agent-state',
+        destination: '/coagents/persistence/loading-agent-state',
+        permanent: true,
+      },
+      {
+        source: '/coagents/concepts/state',
+        destination: '/coagents/shared-state',
+        permanent: true,
+      },
+      {
+        source: '/coagents/concepts/human-in-the-loop',
+        destination: '/coagents/human-in-the-loop',
+        permanent: true,
+      },
+      {
+        source: '/coagents/concepts/multi-agent-flows',
+        destination: '/coagents/multi-agent-flows',
+        permanent: true,
+      },
     ];
   },
 };
