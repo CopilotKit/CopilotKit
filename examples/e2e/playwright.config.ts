f467543e6ddeb26ec4ee503bacc24fce1e95d17e
Ariel Weinberger <weinberger.ariel@gmail.com>
2025-02-11 00:47:11 -0600

    better configs

--- examples/e2e/playwright.config.ts
+++ examples/e2e/playwright.config.ts
@@ -12,16 +12,14 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 100000,
+  timeout: 60000,
   testDir: "./tests",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 6 : 3,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  retries: process.env.CI ? 3 : 3,
+  /* Opt out of parallel tests on non-CI environments. */
+  workers: process.env.CI ? 10 : "90%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["list"],
@@ -40,6 +38,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    video: {
+      mode: "retain-on-failure",
+    }
   },
 
   /* Configure projects for major browsers */
