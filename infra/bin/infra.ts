86264cf30192c3d7c20836dcfdd179a761cb5e63
Ariel Weinberger <Weinberger.Ariel@gmail.com>
2025-02-14 23:16:08 -0600

    e2e cleanup (#1356)

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -14,21 +14,15 @@ const coAgentsResearchCanvasAgentWithLocalDeps = createAgentProjectStack({
   app,
   project: "coagents-research-canvas",
   description: "CoAgents Research Canvas (Agent) - Local Depenencies",
-  dependencies: "Local"
 });
 
 const coAgentsResearchCanvasUIWithLocalDeps = createUIProjectStack({
   app,
   project: "coagents-research-canvas",
   description: "CoAgents Research Canvas (UI) - Local Depenencies",
-  dependencies: "Local",
   selfHostedAgentProject: coAgentsResearchCanvasAgentWithLocalDeps.selfHostedAgent,
   lgcAgentProjectPython: coAgentsResearchCanvasAgentWithLocalDeps.lgcAgentPython,
   lgcAgentProjectJS: coAgentsResearchCanvasAgentWithLocalDeps.lgcAgentJS,
-  environmentVariables: {},
-  customOutputs: {
-    "LgcJSDeploymentUrl": `https://coagents-research-canvas-st-08476feebc3a58e5925116da0d3ad635.default.us.langgraph.app`
-  }
 });
 
 /*
@@ -39,21 +33,15 @@ const coAgentsRoutingAgentWithLocalDeps = createAgentProjectStack({
   app,
   project: "coagents-routing",
   description: "CoAgents Routing (Agent) - Local Dependencies",
-  dependencies: "Local"
 });
 
 const coAgentsRoutingUIWithLocalDeps = createUIProjectStack({
   app,
   project: "coagents-routing",
   description: "CoAgents Routing (UI) - Local Dependencies",
-  dependencies: "Local",
   selfHostedAgentProject: coAgentsRoutingAgentWithLocalDeps.selfHostedAgent,
   lgcAgentProjectPython: coAgentsRoutingAgentWithLocalDeps.lgcAgentPython,
   lgcAgentProjectJS: coAgentsRoutingAgentWithLocalDeps.lgcAgentJS,
-  environmentVariables: {},
-  customOutputs: {
-    "LgcJSDeploymentUrl": `https://coagents-routing-stg-js-4df4be4cab70578ca535df7e1c0b05cf.default.us.langgraph.app`
-  }
 });
 
 /*
@@ -64,21 +52,15 @@ const qaTextAgentWithLocalDeps = createAgentProjectStack({
   app,
   project: "coagents-qa-text",
   description: "CoAgents QA Text (Agent) - Local Dependencies",
-  dependencies: "Local"
 });
 
 const qaTextUIWithLocalDeps = createUIProjectStack({
   app,
   project: "coagents-qa-text",
   description: "CoAgents QA Text (UI) - Local Dependencies",
-  dependencies: "Local",
   selfHostedAgentProject: qaTextAgentWithLocalDeps.selfHostedAgent,
   lgcAgentProjectPython: qaTextAgentWithLocalDeps.lgcAgentPython,
   lgcAgentProjectJS: qaTextAgentWithLocalDeps.lgcAgentJS,
-  environmentVariables: {},
-  customOutputs: {
-    "LgcJSDeploymentUrl": `https://coagents-qa-text-stg-js-4d74616e480750d0a5d10d0c3c5d44a4.default.us.langgraph.app`
-  }
 });
 
 /*
@@ -89,21 +71,15 @@ const qaNativeAgentWithLocalDeps = createAgentProjectStack({
   app,
   project: "coagents-qa-native",
   description: "CoAgents QA Native (Agent) - Local Dependencies",
-  dependencies: "Local"
 });
 
 const qaNativeUIWithLocalDeps = createUIProjectStack({
   app,
   project: "coagents-qa-native",
   description: "CoAgents QA Native (UI) - Local Dependencies",
-  dependencies: "Local",
   selfHostedAgentProject: qaNativeAgentWithLocalDeps.selfHostedAgent,
   lgcAgentProjectPython: qaNativeAgentWithLocalDeps.lgcAgentPython,
   lgcAgentProjectJS: qaNativeAgentWithLocalDeps.lgcAgentJS,
-  environmentVariables: {},
-  customOutputs: {
-    "LgcJSDeploymentUrl": `https://coagents-qa-native-stg-js-036615e530e8593286ccf93d3003ffe2.default.us.langgraph.app`
-  }
 });
 
 /**
