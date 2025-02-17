1f1dc66433cd5c3210c0c5151f1907c25ec05bff
Ran Shem Tov <ran@copilotkit.ai>
2025-02-17 18:12:47 +0100

    fix: refine argument types

--- CopilotKit/packages/react-core/src/types/interrupt-action.ts
+++ CopilotKit/packages/react-core/src/types/interrupt-action.ts
@@ -1,23 +1,27 @@
 import { LangGraphInterruptEvent } from "@copilotkit/runtime-client-gql";
 import { AgentSession } from "../context/copilot-context";
 
+export interface LangGraphInterruptRenderHandlerProps<TEventValue = any> {
+  event: LangGraphInterruptEvent<TEventValue>;
+  resolve: (resolution: string) => void;
+}
+
+export interface LangGraphInterruptRenderProps<TEventValue = any> {
+  result: unknown;
+  event: LangGraphInterruptEvent<TEventValue>;
+  resolve: (resolution: string) => void;
+}
+
 export interface LangGraphInterruptRender<TEventValue = any> {
   id: string;
   /**
    * The handler function to handle the event.
    */
-  handler?: (props: {
-    event: LangGraphInterruptEvent<TEventValue>;
-    resolve: (resolution: string) => void;
-  }) => unknown | Promise<unknown>;
+  handler?: (props: LangGraphInterruptRenderHandlerProps<TEventValue>) => any | Promise<any>;
   /**
    * The render function to handle the event.
    */
-  render?: (props: {
-    result: unknown;
-    event: LangGraphInterruptEvent<TEventValue>;
-    resolve: (resolution: string) => void;
-  }) => string | React.ReactElement;
+  render?: (props: LangGraphInterruptRenderProps<TEventValue>) => string | React.ReactElement;
   /**
    * Method that returns a boolean, indicating if the interrupt action should run
    * Useful when using multiple interrupts
