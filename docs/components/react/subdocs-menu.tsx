b026e4c7629ec5055017205cf5f3d4764723760c
Markus Ecker <markus.ecker@gmail.com>
2025-02-24 15:32:03 +0000

    framework selector dropdown

--- docs/components/react/subdocs-menu.tsx
+++ docs/components/react/subdocs-menu.tsx
@@ -5,13 +5,22 @@ import {
   type ReactNode,
   useCallback,
   useMemo,
+  useRef,
   useState,
 } from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { useSidebar } from "fumadocs-ui/provider";
 import { SearchToggle } from "../layout/top-bar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { PiGraph } from "react-icons/pi";
 
 export function isActive(
   url: string,
@@ -38,27 +47,54 @@ export interface Option {
   props?: HTMLAttributes<HTMLElement>;
 }
 
+export interface OptionDropdown {
+  title: ReactNode;
+  options: Option[];
+}
+
+function isOptionDropdown(
+  item: Option | OptionDropdown
+): item is OptionDropdown {
+  return "options" in item;
+}
+
+function isOption(item: Option | OptionDropdown): item is Option {
+  return !isOptionDropdown(item);
+}
+
 export function SubdocsMenu({
   options,
   ...props
 }: {
-  options: Option[];
+  options: (Option | OptionDropdown)[];
 } & HTMLAttributes<HTMLButtonElement>): React.ReactElement {
   const { closeOnRedirect } = useSidebar();
   const pathname = usePathname();
-  const selected = useMemo(() => {
+  const selected: Option | undefined = useMemo(() => {
     // First, try all non-root options
-    const nonRootOptions = options.filter((item) => item.url !== "/");
-    const activeNonRootOption = nonRootOptions.find((item) =>
-      isActive(item.url, pathname, true)
+    let nonRootOptions = options.filter(
+      (item) => isOption(item) && item.url !== "/"
+    );
+
+    const dropDowns = options.filter((item) => isOptionDropdown(item));
+
+    if (dropDowns.length > 0) {
+      const dropDown = dropDowns[0];
+      nonRootOptions = nonRootOptions.concat(dropDown.options);
+    }
+
+    const activeNonRootOption = nonRootOptions.find(
+      (item) => isOption(item) && isActive(item.url, pathname, true)
     );
 
     if (activeNonRootOption) {
-      return activeNonRootOption;
+      return activeNonRootOption as Option;
     }
 
     // If no non-root options are active, try the root options ("/*")
-    return options.find((item) => isActive(item.url, pathname, true, true));
+    return options.find(
+      (item) => isOption(item) && isActive(item.url, pathname, true, true)
+    ) as Option | undefined;
   }, [options, pathname]);
 
   const onClick = useCallback(() => {
@@ -68,29 +104,135 @@ export function SubdocsMenu({
   return (
     <div className="flex flex-col gap-2 border-b p-4">
       {options.map((item) => (
-        <Link
-          key={item.url}
-          href={item.url}
+        <SubdocsMenuItem
+          key={isOption(item) ? item.url : "dropdown"}
+          item={item}
+          selected={selected}
           onClick={onClick}
-          {...item.props}
-          className={cn(
-            "p-2 flex flex-row gap-3 items-center cursor-pointer group opacity-60 hover:opacity-100",
-            item.props?.className,
-            selected === item && `${item.selectedStyle} opacity-100`,
-          )}
-        >
-          <div
-            className={cn(
-              "rounded-sm p-1.5",
-              item.bgGradient,
-              selected !== item && "",
-            )}
-          >
-            {item.icon}
-          </div>
-          <div className="font-medium">{item.title}</div>
-        </Link>
+        />
       ))}
     </div>
+  );
+}
+
+function SubdocsMenuItem({
+  item,
+  selected,
+  onClick,
+}: {
+  item: Option | OptionDropdown;
+  selected?: Option;
+  onClick?: () => void;
+}) {
+  if (isOption(item)) {
+    return (
+      <Link
+        key={item.url}
+        href={item.url}
+        onClick={onClick}
+        {...item.props}
+        className={cn(
+          "p-2 flex flex-row gap-3 items-center cursor-pointer group opacity-60 hover:opacity-100",
+          item.props?.className,
+          selected === item && `${item.selectedStyle} opacity-100`
+        )}
+      >
+        <div
+          className={cn(
+            "rounded-sm p-1.5",
+            item.bgGradient,
+            selected !== item && ""
+          )}
+        >
+          {item.icon}
+        </div>
+        <div className="font-medium">{item.title}</div>
+      </Link>
+    );
+  } else if (isOptionDropdown(item)) {
+    return (
+      <SubdocsMenuItemDropdown
+        item={item}
+        selected={selected}
+        onClick={onClick}
+      />
+    );
+  }
+}
+
+function SubdocsMenuItemDropdown({
+  item,
+  selected,
+  onClick,
+}: {
+  item: OptionDropdown;
+  selected?: Option;
+  onClick?: () => void;
+}) {
+  const router = useRouter();
+  const selectRef = useRef(null);
+
+  const selectedOption = item.options.find(
+    (option) => option.url === selected?.url
+  );
+
+  const isSelected = selectedOption !== undefined;
+
+  return (
+    <Select
+      onValueChange={(url) => {
+        router.push(url);
+        onClick?.();
+        if (selectRef.current) {
+          setTimeout(() => {
+            (selectRef.current as any).blur();
+          }, 10);
+        }
+      }}
+      value={selectedOption?.url}
+    >
+      <SelectTrigger
+        className={cn(
+          "pl-[2px] pt-0 border-0 h-11",
+          isSelected
+            ? "ring-purple-500/70 ring-2 rounded-sm"
+            : "ring-0 opacity-60"
+        )}
+        ref={selectRef}
+      >
+        <SelectValue
+          placeholder={
+            <div className="flex pl-0 justify-center items-center">
+              <div className={cn("rounded-sm p-1.5 mr-1")}>
+                <PiGraph
+                  className={cn(
+                    "w-7 h-7 text-bold p-1.5 bg-gradient-to-b rounded-sm",
+                    "from-purple-700 to-purple-400 text-purple-100 inline-block"
+                  )}
+                />
+              </div>
+              CoAgents
+            </div>
+          }
+        />
+      </SelectTrigger>
+      <SelectContent>
+        {item.options.map((option) => (
+          <SelectItem key={option.url} value={option.url}>
+            <div className="flex pl-0 justify-center items-center">
+              <div className={cn("rounded-sm p-1.5 mr-1")}>
+                <PiGraph
+                  className={cn(
+                    "w-7 h-7 text-bold p-1.5 bg-gradient-to-b rounded-sm",
+                    "from-purple-700 to-purple-400 text-purple-100 inline-block"
+                  )}
+                />
+              </div>
+              {option.title}
+            </div>
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 }
