748038994b73a4d9ed047edf6c9418697228d4a7
Ran Shem Tov <ran@copilotkit.ai>
2025-02-26 16:41:38 +0100

    prettier

--- CopilotKit/packages/react-ui/src/components/chat/Markdown.tsx
+++ CopilotKit/packages/react-ui/src/components/chat/Markdown.tsx
@@ -26,12 +26,12 @@ export const Markdown = ({ content }: MarkdownProps) => {
 
 const components: Components = {
   p({ children }) {
-    return <p style={{ maxWidth: '80%' }}>{children}</p>;
+    return <p style={{ maxWidth: "80%" }}>{children}</p>;
   },
   a({ children, ...props }) {
     return (
       <a
-        style={{ color: "blue", textDecoration: "underline", maxWidth: '80%' }}
+        style={{ color: "blue", textDecoration: "underline", maxWidth: "80%" }}
         {...props}
         target="_blank"
         rel="noopener noreferrer"
