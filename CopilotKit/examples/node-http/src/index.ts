b8d78ef1102b13b6a5768cc7035f45298052c3de
Muhammad Abubakar <abubakaran102025@gmail.com>
2025-03-06 17:55:34 +0500

    Added dotenv Imports in node-express and node-http (#1456)

--- CopilotKit/examples/node-http/src/index.ts
+++ CopilotKit/examples/node-http/src/index.ts
@@ -1,6 +1,8 @@
 import { createServer } from "node:http";
 import { CopilotRuntime, OpenAIAdapter, copilotRuntimeNodeHttpEndpoint } from "@copilotkit/runtime";
 import OpenAI from "openai";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const openai = new OpenAI();
 const serviceAdapter = new OpenAIAdapter({ openai });
