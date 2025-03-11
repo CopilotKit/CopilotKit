aacb5495334970bbaa30b039b9d32db62f8da475
Markus Ecker <markus.ecker@gmail.com>
2025-03-11 15:07:11 +0100

    add agents

---
+++ examples/crewai/agent/demo/tool_based_generative_ui.py
@@ -0,0 +1,81 @@
+"""
+An example demonstrating tool-based generative UI.
+"""
+
+from crewai.flow.flow import Flow, start
+from copilotkit.crewai import copilotkit_stream, CopilotKitState
+from litellm import completion
+
+
+# This tool generates a haiku on the server.
+# The tool call will be streamed to the frontend as it is being generated.
+GENERATE_HAIKU_TOOL = {
+    "type": "function",
+    "function": {
+        "name": "generate_haiku",
+        "description": "Generate a haiku in Japanese and its English translation",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "japanese": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    },
+                    "description": "An array of three lines of the haiku in Japanese"
+                },
+                "english": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    },
+                    "description": "An array of three lines of the haiku in English"
+                }
+            },
+            "required": ["japanese", "english"]
+        }
+    }
+}
+
+
+class ToolBasedGenerativeUIFlow(Flow[CopilotKitState]):
+
+  @start()
+  async def chat(self):
+    system_prompt = f"You assist the user in generating a haiku."
+
+    # 1. Run the model and stream the response
+    #    Note: In order to stream the response, wrap the completion call in
+    #    copilotkit_stream and set stream=True.
+    response = await copilotkit_stream(
+        completion(
+
+            # 1.1 Specify the model to use
+            model="openai/gpt-4o",
+            messages=[
+                {
+                    "role": "system", 
+                    "content": system_prompt
+                },
+                *self.state.messages
+            ],
+
+            # 1.2 Bind the available tools to the model
+            tools=[
+                *self.state.copilotkit.actions,
+                GENERATE_HAIKU_TOOL,
+            ],
+
+            # 1.3 Disable parallel tool calls to avoid race conditions,
+            #     enable this for faster performance if you want to manage
+            #     the complexity of running tool calls in parallel.
+            parallel_tool_calls=False,
+            stream=True
+        )
+    )
+
+    message = response.choices[0].message
+
+    # 2. Append the message to the messages in state
+    self.state.messages.append(message)
+
