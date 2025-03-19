9a89d88611aee5e4553defba664252f7ed2584e9
Tyler Slaton <tyler@copilotkit.ai>
2025-03-19 18:31:22 -0400

    wip
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

---
+++ registry/components/runtime/langgraph-platform.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from "next/server";
+import {
+  CopilotRuntime,
+  copilotRuntimeNextJSAppRouterEndpoint,
+  ExperimentalEmptyAdapter,
+  langGraphPlatformEndpoint,
+} from "@copilotkit/runtime";
+
+const serviceAdapter = new ExperimentalEmptyAdapter();
+
+const runtime = new CopilotRuntime({
+  remoteEndpoints: [
+    langGraphPlatformEndpoint({
+      deploymentUrl: process.env.LANGGRAPH_DEPLOYMENT_URL || "",
+      langsmithApiKey: process.env.LANGSMITH_API_KEY || "",
+      agents: [
+        {
+          name: process.env.NEXT_PUBLIC_COPILOTKIT_AGENT_NAME || "",
+          description: process.env.NEXT_PUBLIC_COPILOTKIT_AGENT_DESCRIPTION || "",
+        },
+      ],
+    }),
+  ],
+});
+
+export const POST = async (req: NextRequest) => {
+  const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+    runtime,
+    serviceAdapter,
+    endpoint: "/api/copilotkit",
+  });
+
+  return handleRequest(req);
+};
