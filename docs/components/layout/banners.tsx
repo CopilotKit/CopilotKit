c09fbddb5a698723e0832bdf2bf464d764f05b54
Tyler Slaton <tyler@copilotkit.ai>
2025-03-24 15:11:21 -0400

    docs: add migration guide for 1.8.0
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

---
+++ docs/components/layout/banners.tsx
@@ -0,0 +1,23 @@
+import { Banner } from "fumadocs-ui/components/banner";
+import Link from "next/link";
+import { PaintbrushIcon } from "lucide-react";
+export function Banners() {
+  return (
+    <>
+      {/* <NewLookAndFeelBanner /> */}
+      {/* Add other banners here */}
+    </>
+  )
+}
+
+export function NewLookAndFeelBanner() {
+  return (
+    <Banner className="w-full text-white gap-2 bg-indigo-500 dark:bg-indigo-900" variant="rainbow" id="new-look-and-feel-banner">
+      <PaintbrushIcon className="w-5 h-5" />
+      <p>
+        We are launching a new default look and feel!
+        Checkout the <span className="underline"><Link href="/migration-guide"> migration guide </Link></span> to learn more.
+      </p>
+    </Banner>
+  )
+}
