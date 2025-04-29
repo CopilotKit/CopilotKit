67cf12113087757a54b241c0aa2787eb13653b90
Tyler Slaton <tyler@copilotkit.ai>
2025-04-29 14:08:55 -0400

    feat(docs): add banner for mcp launch
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- docs/components/layout/banners.tsx
+++ docs/components/layout/banners.tsx
@@ -1,11 +1,13 @@
 import { Banner } from "fumadocs-ui/components/banner";
 import Link from "next/link";
 import { PaintbrushIcon } from "lucide-react";
+import { PiGraph } from "react-icons/pi";
 import { SiCrewai } from "@icons-pack/react-simple-icons";
+
 export function Banners() {
   return (
     <>
-      <CoagentsCrewAnnouncementBanner />
+      <ModelContextProtocolBanner />
     </>
   )
 }
@@ -26,6 +28,16 @@ export function CoagentsCrewAnnouncementBanner() {
   return (
     <Banner className="w-full text-white gap-2 bg-indigo-500 dark:bg-indigo-900" variant="rainbow" id="coagents-crew-announcement-banner">
       <SiCrewai className="w-5 h-5 inline mb-1" /> CrewAI support is here! Checkout the <Link href="/crewai-crews" className="underline">Crew</Link> and <Link href="/crewai-flows" className="underline">Flow</Link> documentation.
+    </Banner>
+  )
+}
+
+export function ModelContextProtocolBanner() {
+  return (
+    <Banner className="w-full text-white bg-indigo-500 dark:bg-indigo-900 h-24 sm:h-14 !important" variant="rainbow" id="model-context-protocol-banner">
+      <p className="w-3/4">
+        <PiGraph className="w-5 h-5 inline mr-2" /> Model Context Protocol (MCP) support is here! Try it out <Link href="/guides/model-context-protocol" className="underline">here</Link>. Register to our<Link href="https://go.copilotkit.ai/webinarMastra" target="_blank" className="underline ml-1">webinar</Link> for a walkthrough.
+      </p>
     </Banner>
   )
 }
