dfdcd119ef29d4a9ac307e31038066ea290c4a16
Markus Ecker <markus.ecker@gmail.com>
2025-05-12 10:26:01 +0200

    remove comments

--- examples/ag2/feature-viewer/src/app/api/copilotkit/route.ts
+++ examples/ag2/feature-viewer/src/app/api/copilotkit/route.ts
@@ -12,11 +12,11 @@ const AGENTIC_CHAT_AGENT_URL = process.env.AGENTIC_CHAT_AGENT_URL;
 const HUMAN_IN_THE_LOOP_AGENT_URL = process.env.HUMAN_IN_THE_LOOP_AGENT_URL;
 
 const agenticChatAgent = new HttpAgent({
-  url: AGENTIC_CHAT_AGENT_URL!, // "https://copilotkit-dojo.ag2.ai:8000/fastagency/awp",
+  url: AGENTIC_CHAT_AGENT_URL!,
 });
 
 const humanInTheLoopAgent = new HttpAgent({
-  url: HUMAN_IN_THE_LOOP_AGENT_URL!, // "https://copilotkit-dojo.ag2.ai:8008/fastagency/awp",
+  url: HUMAN_IN_THE_LOOP_AGENT_URL!,
 });
 
 const runtime = new CopilotRuntime({
