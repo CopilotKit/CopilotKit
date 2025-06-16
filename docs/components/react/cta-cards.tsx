34d8c2e1198dcece38f61f10d0e2bc97d0e4cb1b
johnprg <john@theartofwork.com>
2025-06-15 21:02:15 -0600

    use client

--- docs/components/react/cta-cards.tsx
+++ docs/components/react/cta-cards.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import { IconType } from "react-icons";
