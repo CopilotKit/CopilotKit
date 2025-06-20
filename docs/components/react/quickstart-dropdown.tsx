a509da923a0ac9e86248c606e196efcaceb49f3e
johnprg <john@theartofwork.com>
2025-06-20 14:16:44 -0700

    fixed resizing button issue

--- docs/components/react/quickstart-dropdown.tsx
+++ docs/components/react/quickstart-dropdown.tsx
@@ -27,7 +27,7 @@ export function QuickstartDropdown() {
   return (
     <select
       onChange={handleSelectChange}
-      className="text-indigo-800 dark:text-indigo-300 ring-1 ring-indigo-200 dark:ring-indigo-900 text-sm items-center bg-gradient-to-r from-indigo-200/50 to-purple-200/80 dark:from-indigo-900/40 dark:to-purple-900/50 p-3 px-4 transition-all duration-100 hover:ring-2 hover:ring-indigo-400 hover:dark:text-indigo-200 rounded-lg no-underline cursor-pointer w-full"
+      className="text-indigo-800 dark:text-indigo-300 ring-1 ring-indigo-200 dark:ring-indigo-900 text-sm items-center bg-gradient-to-r from-indigo-200/50 to-purple-200/80 dark:from-indigo-900/40 dark:to-purple-900/50 p-3 px-4 transition-all duration-100 hover:ring-2 hover:ring-indigo-400 hover:dark:text-indigo-200 rounded-lg no-underline cursor-pointer w-auto"
       defaultValue="" // Set a default empty value
     >
       <option value="" disabled>
