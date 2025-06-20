69edfabb05e101618c844448f8eee36c35375e33
Suhas Deshpande <suhas2u@gmail.com>
2025-06-20 09:30:38 -0700

    fix linter

--- CopilotKit/packages/react-core/jest.config.js
+++ CopilotKit/packages/react-core/jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
+  setupFilesAfterEnv: [],
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.(ts|tsx)",
+    "<rootDir>/src/**/*.(test|spec).(ts|tsx)",
+  ],
 };
