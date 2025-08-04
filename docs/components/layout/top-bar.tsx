2f2e3cda1ca7866d561c21e3c8a7fa496e06436b
johnprg <john@theartofwork.com>
2025-08-04 15:50:20 -0700

    Revert "replace 'copilot cloud' with 'copilotkit platform'"
    This reverts commit 2a30bd05e44effa03aa0cd43320336471b5ea88d.

--- docs/components/layout/top-bar.tsx
+++ docs/components/layout/top-bar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { SearchIcon } from "lucide-react";
-import { LinkToCopilotCloud } from "@/components/react/link-to-copilotkit-platform";
+import { LinkToCopilotCloud } from "@/components/react/link-to-copilot-cloud";
 
 export function TopBar() {
   return (
