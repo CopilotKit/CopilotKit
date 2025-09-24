aa0005c648053238bff38f4e62d9f81a25f63ace
johnprg <john@theartofwork.com>
2025-09-24 12:06:00 -0700

    Revert "Docs/rebranding (#2455)"
    This reverts commit f612cd5477f4ea82a725574d00c8932772513353.

--- docs/components/layout/banners.tsx
+++ docs/components/layout/banners.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Banner } from "fumadocs-ui/components/banner";
 import Link from "next/link";
 import { PaintbrushIcon } from "lucide-react";
@@ -48,7 +46,7 @@ export function AGUIBanner() {
   return (
     <Banner className="w-full text-white bg-indigo-500 dark:bg-indigo-900 h-24 sm:h-14 !important" variant="rainbow" id="agui-banner">
       <p className="w-3/4">
-        CopilotKit and our framework partners have launched the AG-UI (Agent-User Interaction) protocol! <Link href="/ag-ui-protocol" target="_blank" className="underline" rel="noopener noreferrer">Learn more</Link>.
+        CopilotKit and our framework partners have launched the AG-UI protocol for agent-user interaction! <Link href="/ag-ui-protocol" target="_blank" className="underline" rel="noopener noreferrer">Learn more</Link>.
       </p>
     </Banner>
   )
