763755e2b9e9258deb88b6f313bf48bf09a290c3
johnprg <john@theartofwork.com>
2025-09-30 11:01:10 -0700

    Revert "Docs/dark mode fixes (#2533)"
    This reverts commit d24cffe7e1f1a02f77d132f9f66b7a784aae9016.

--- docs/components/ui/button.tsx
+++ docs/components/ui/button.tsx
@@ -12,13 +12,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-lg shadow-black/10 dark:shadow-white/10 hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-md shadow-black/10 dark:shadow-white/10 hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-md shadow-black/10 dark:shadow-white/20 hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-md shadow-black/10 dark:shadow-white/10 hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
