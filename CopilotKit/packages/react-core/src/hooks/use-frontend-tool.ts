cd19946b489e3aac61a087ae8ddcfdc30d92af75
Ran Shemtov <ran@copilotkit.ai>
2025-10-07 20:58:49 +0200

    fix: allow disabling actions in the new action hooks (#2582)

--- CopilotKit/packages/react-core/src/hooks/use-frontend-tool.ts
+++ CopilotKit/packages/react-core/src/hooks/use-frontend-tool.ts
@@ -2,11 +2,15 @@ import { useCopilotAction } from "./use-copilot-action";
 import { FrontendAction } from "../types/frontend-action";
 import { Parameter } from "@copilotkit/shared";
 
+type UseToolCallArgs<T extends Parameter[] | [] = []> = {
+  available?: "disabled" | "enabled";
+} & Pick<
+  FrontendAction<T>,
+  "name" | "description" | "parameters" | "handler" | "followUp" | "render"
+>;
+
 export function useFrontendTool<const T extends Parameter[] | [] = []>(
-  tool: Pick<
-    FrontendAction<T>,
-    "name" | "description" | "parameters" | "handler" | "followUp" | "render"
-  >,
+  tool: UseToolCallArgs<T>,
   dependencies?: any[],
 ) {
   // Use the existing useCopilotAction hook
--- CopilotKit/packages/react-core/src/hooks/use-human-in-the-loop.ts
+++ CopilotKit/packages/react-core/src/hooks/use-human-in-the-loop.ts
@@ -2,10 +2,13 @@ import { useCopilotAction } from "./use-copilot-action";
 import { FrontendAction } from "../types";
 import { Parameter } from "@copilotkit/shared";
 
+type UseToolCallArgs<T extends Parameter[] | [] = []> = {
+  available?: "disabled" | "enabled";
+  render: FrontendAction<T>["renderAndWaitForResponse"];
+} & Pick<FrontendAction<T>, "name" | "description" | "parameters">;
+
 export function useHumanInTheLoop<const T extends Parameter[] | [] = []>(
-  tool: Pick<FrontendAction<T>, "name" | "description" | "parameters"> & {
-    render: FrontendAction<T>["renderAndWaitForResponse"];
-  },
+  tool: UseToolCallArgs<T>,
   dependencies?: any[],
 ) {
   const { render, ...toolRest } = tool;
@@ -13,7 +16,7 @@ export function useHumanInTheLoop<const T extends Parameter[] | [] = []>(
   useCopilotAction(
     {
       ...toolRest,
-      available: "remote",
+      available: tool.available === "disabled" ? tool.available : "remote",
       renderAndWaitForResponse: render,
     },
     dependencies,
