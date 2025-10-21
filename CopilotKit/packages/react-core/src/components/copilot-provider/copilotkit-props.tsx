b160e16cf5d3ec4a494817810d0a6d777a1aa6e4
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-10-21 04:47:48 -0400

    fix: silence build errors

--- CopilotKit/packages/react-core/src/components/copilot-provider/copilotkit-props.tsx
+++ CopilotKit/packages/react-core/src/components/copilot-provider/copilotkit-props.tsx
@@ -7,7 +7,13 @@ import { CopilotKitProviderProps } from "@copilotkitnext/react";
  * Props for CopilotKit.
  */
 
-export interface CopilotKitProps extends CopilotKitProviderProps {
+/**
+ * We shouldn't need this `Omit` here, but using it because `CopilotKitProps`
+ * and `CopilotKitProviderProps` have non-identical `children` types
+ * 
+ * TODO: Remove this `Omit` once this is resolved.
+ */
+export interface CopilotKitProps extends Omit<CopilotKitProviderProps, "children"> {
   /**
    * Your Copilot Cloud API key.
    *
