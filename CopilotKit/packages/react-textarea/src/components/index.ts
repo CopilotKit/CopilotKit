8714bb162c42e04ec068039d0e075c929f91d6e6
Ran Shem Tov <ran@copilotkit.ai>
2025-11-25 12:08:12 +0100

    chore: export backwards compatible text area

--- CopilotKit/packages/react-textarea/src/components/index.ts
+++ CopilotKit/packages/react-textarea/src/components/index.ts
@@ -1,1 +1,4 @@
 export { BaseCopilotTextarea } from "./base-copilot-textarea/base-copilot-textarea";
+
+export { CopilotTextarea } from "./copilot-textarea/copilot-textarea";
+export type { CopilotTextareaProps } from "./copilot-textarea/copilot-textarea";
