ef095302ee5ec0ae4a2a8c28535b210d9cadadd5
Billy Bautista <billy.bautista.cornelio@gmail.com>
2025-12-12 17:36:24 -0300

    feat: Improved icon opacity transition

--- docs/components/react/custom-accordion.tsx
+++ docs/components/react/custom-accordion.tsx
@@ -11,17 +11,22 @@ export function CustomAccordion({
   children,
   icon,
 }: CustomAccordionProps) {
+  const value = title.toLowerCase().replace(/\s+/g, "-");
+
   return (
     <Accordion
+      value={value}
       title={
         <div className="flex items-center gap-4">
-          {icon}
+          <div className="opacity-60 group-hover:opacity-100 group-data-[state=open]:opacity-100 transition-opacity duration-300">
+            {icon}
+          </div>
           <div className="text-base font-medium! text-[#010507] dark:text-white">
             {title}
           </div>
         </div>
       }
-      className="bg-[#FFFFFF80] border-none! accordion-icon-right dark:bg-[#01050780] rounded-lg hover:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF0D] transition-all duration-300"
+      className="group bg-[#FFFFFF80] border-none! accordion-icon-right dark:bg-[#01050780] rounded-lg hover:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF0D] transition-all duration-300"
     >
       {children}
     </Accordion>
