9486eeb39a514b3199ecdb6a3f1f9bb48d99233c
Tyler Slaton <tyler@copilotkit.ai>
2025-12-31 12:46:01 -0500

    refactor: update chat-with-your-data and form-filling
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- examples/v1.x/form-filling/app/layout.tsx
+++ examples/v1.x/form-filling/app/layout.tsx
@@ -26,11 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <CopilotKit 
-          publicApiKey={process.env.NEXT_PUBLIC_COPILOT_PUBLIC_API_KEY}
-          showDevConsole={false}
-        >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <CopilotKit runtimeUrl="/api/copilotkit" showDevConsole={false}>
           {children}
         </CopilotKit>
       </body>
