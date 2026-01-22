0a6fb120848b2408f861c8ccd569f57472620350
Tyler Slaton <54378333+tylerslaton@users.noreply.github.com>
2026-01-22 17:54:41 -0500

    fix: address issue with lint failing due to version incompatibilities (#3082)
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- examples/v1.x/state-machine/eslint.config.mjs
+++ examples/v1.x/state-machine/eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  }
 ];
 
 export default eslintConfig;
