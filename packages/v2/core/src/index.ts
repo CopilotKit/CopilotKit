4c52f6a1b6e789c9eb47ee74e0d12dc6049c73bc
Benjamin Taylor <ben@liveloveapp.com>
2026-03-06 11:47:10 -0600

    feat(runtime, react): Wire in live updates for threads in useThreads

--- packages/v2/core/src/index.ts
+++ packages/v2/core/src/index.ts
@@ -4,5 +4,3 @@ export * from "./agent";
 export * from "./utils/markdown";
 export * from "./intelligence-agent";
 export * from "./utils/micro-redux";
-export * from "./utils/phoenix-observable";
-export * from "./threads";
